50f182b0726dd62ffa61b22caa0955ced0f2d473
mdeyanov83 <mdeyanov@gmail.com>
2025-08-15 00:07:54 +0000

    Fri, Aug 15, 2025, 12:07 AM +00:00

--- dart/course/bin/course.dart
+++
@@ -1,5 +0,0 @@
-import 'package:course/course.dart' as course;
-
-void main(List<String> arguments) {
-  print('Hello world: ${course.calculate()}!');
-}
--- dart/course/test/course_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:course/course.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
