39176c49a5c26a6c5596a584946de21915eeee5b
mdeyanov83 <mdeyanov@gmail.com>
2025-08-15 00:05:46 +0000

    Fri, Aug 15, 2025, 12:05 AM +00:00

--- dart/course/bin/course.dart
+++ dart/course/bin/course.dart
@@ -1,5 +1,5 @@
 import 'package:course/course.dart' as course;
 
 void main(List<String> arguments) {
-  prit('Hello world: ${course.calculate()}!');
+  print('Hello world: ${course.calculate()}!');
 }
