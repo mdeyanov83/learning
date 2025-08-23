94b95eae63b430acf4cfe92e9aed5f9df1e95d30
mdeyanov83 <mdeyanov@gmail.com>
2025-08-23 21:35:21 +0000

    Sat, Aug 23, 2025, 9:35 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_I4.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_I4.dart
@@ -1,22 +1,97 @@
 //! Classes - Methods
+// Methods -> are functions defined inside a class
 
 //! Constructors
+// The method that is called when a class is instantiated into an object
+// every class comes with a default constructor - a function with no name, no body and the same name as the class
+// The constructor doesn't need a function body it can exist in just a header form (although may have one if necessary)
+// Do not have a return type
+// Their purpose is so that the fields inside a class can be rapidly initialized in a more organized fashion
 
-as
-as
-dsf
+// When instantiating a class object, the fields inside are first assigned the values provided upon declaration,
+// THEN the constructor is called
+
+// static variables cannot be set inside of a constructor, as they exist in the class itself, without an object instance
+
+
+
+
+class A {
+
+  // 4:38:45 - start of constructor function parameters explanation
+
+  // A(); //! constructor in just a header form, no body
+
+  A(
+    int p,
+    int a,
+    int b,
+    int c,
+    int d,
+    int e,
+    int f,
+  ) {
+    _private = p;
+    a = a;
+    b = b;
+    c = c;
+    d = d;
+    e = e;
+    f = f;
+  }
+
+
+
+  int? _private; // private field
+
+  int? a;
+  int b = 1;
+
+  final int c = 2;
+
+  late int d;
+  late final int e;
+  late final int f = 5;
+
+  static int g = 6;
+  static late int h;
+  static late int i = 8; // lazy initialization, in this case pointless
+  static late final int j;
+
+  static const int k = 10;
+
+  @override // override the default print(classInstance) method - toString, for easier fields output
+  String toString() =>
+    'A(_private: $_private, a: $a, b: $b, c:, $c, d: $d, e: $e, f: $f)';
+}
 
 void main(List<String> args) {
-a
-df
-ads
-faasdf
-ads
 
-adsf
+  var alfa = A(1, 2, 3, 4, 5, 6, 7);
 
-dsf
-a
-dsf
+  // Rule 1 - accessing vie the dot . operator (the default getter function)
+  print('alfa._private --> ${alfa._private}');
+  print('alfa.a --> ${alfa.a}');
+  print('alfa.b --> ${alfa.b}');
+  print('alfa.c --> ${alfa.c}');
+  alfa.d = 3; // otherwise d is late and not assigned a value -> Unhandled exception
+  print('alfa.d --> ${alfa.d}');
+  alfa.e = 4; // same, but can only be set once, as it is final
+  // alfa.e = 7; //! unhandled exception
+  print('alfa.e --> ${alfa.e}');
+  // alfa.f = 10; //! compile error, final and already assigned
+  print('alfa.f --> ${alfa.f}');
+
+  print('A.g --> ${A.g}');
+
+  A.h = 7;
+  print('A.h --> ${A.h}');
+  print('A.i --> ${A.i}');
+
+  A.j = 9;
+  print('A.j --> ${A.j}');
+  print('A.k --> ${A.k}');
+
+  print(alfa);
 
 }
