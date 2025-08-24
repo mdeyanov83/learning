22cad27aa7dbce5a9ecec08f7ea70e7e04a6873d
mdeyanov83 <mdeyanov@gmail.com>
2025-08-24 22:28:16 +0000

    Sun, Aug 24, 2025, 10:28 PM +00:00

---
+++ dart/flutterly_dart_course/console_project/bin/classes_I9.dart
@@ -0,0 +1,54 @@
+//! Classes
+
+//! Instance Methods
+//* They are normal functions serving specific purposes in a class
+
+
+
+
+
+import 'dart:math';
+
+class Point {
+  const Point({
+    required this.x,
+    required this.y,
+  });
+
+  num distanceTo(Point p) {
+    var dx = x - p.x;
+    var dy = y - p.y;
+    return sqrt(pow(dx, 2) + pow(dy, 2));
+  }
+
+  factory Point.random({required bool isPositive}) {
+    int minNegativeValue = -99;
+    int maxNegativeValue = -1;
+    int minPositiveValue = 0;
+    int maxPositiveValue = 99;
+
+    int randomNegativeValue = minNegativeValue + Random().nextInt(maxNegativeValue - minNegativeValue);
+    int randomPositiveValue = minPositiveValue + Random().nextInt(maxPositiveValue - minPositiveValue);
+
+    return isPositive
+      ? Point(x: randomPositiveValue, y: randomPositiveValue)
+      : Point(x: randomNegativeValue, y: randomNegativeValue);
+  }
+
+  final int x;
+  final int y;
+  static const Point origin = Point(x: 0, y: 0);
+
+  @override
+  String toString() => 'A(x: $x, y: $y)';
+}
+
+void main() {
+
+  var p1 = Point(x: 0, y: 0);
+  var p2 = Point(x: 1, y: 1);
+
+  // p1.distanceTo(p2);
+  print('p1.distanceTo(p2) --> ${p1.distanceTo(p2)}');
+
+}
