d17012f24719ba2d389aaf0af8d8aa5e41e60aea
mdeyanov83 <mdeyanov@gmail.com>
2025-08-24 15:05:44 +0000

    Sun, Aug 24, 2025, 3:05 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_I6.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_I6.dart
@@ -1,6 +1,10 @@
 //! Classes
 
 //! Constant Constructors
+// Imagine if we have a cartesian coordinate system and a class to represent points with x, y coordinates
+// If we create multiple points with the same coordinates, we dont want to have multiple objects representing
+// them in memory. We want only 1 object for efficiency
+
 
 class Point {
   Point({
@@ -15,7 +19,13 @@ class Point {
   String toString() => 'A(x: $x, y: $y)';
 }
 
-void main(List<String> args) {
+void main() {
 
-  }
+  var p1 = Point(x: 1, y: 1);
+  var p2 = Point(x: 1, y: 1);
+  // identical(p1, p2);
+  print('identical(p1, p2) --> ${identical(p1, p2)}');
+
+
+}
 
