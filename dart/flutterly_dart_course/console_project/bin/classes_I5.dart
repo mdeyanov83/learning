dd319ec9ba56f5f706ee287141c33a90b6d23bd1
mdeyanov83 <mdeyanov@gmail.com>
2025-08-24 14:39:17 +0000

    Sun, Aug 24, 2025, 2:39 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_I5.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_I5.dart
@@ -2,24 +2,44 @@
 //* Most of the time in Dart and Flutter you will have mainly a bunch of final fields, that will be initialized inside the constructor
 
 //! Named Constructors
+// We can have multiple constructors inside a class, besides the default one
+//! Named constructor
+//* ClassName.constructorName()
+//* Named constructors are called as a class method.
+// Often named constructors are used when trying to parse JSON data into one of your classes
 
 class A {
   A({
-    required this.a,
-    required this.b,
+    required this.x,
+    required this.y,
   });
 
-  final int a;
-  final int b;
+  A.zero() // Named constructor
+    : x = 0,
+      y = 0;
+
+  A.fromJson({required Map<String, int> json})
+    : x = json['x']!,
+      y = json['y']!;
+
+  final int x;
+  final int y;
 
   @override
-  String toString() => 'A(a: $a, b: $b)';
+  String toString() => 'A(x: $x, y: $y)';
 }
 
 
 void main(List<String> args) {
 
-  var alfa = A(a: 1, b: 2);
+  var alfa = A(x: 1, y: 2);
   print(alfa);
+
+  var alfaZero = A.zero(); // calling the name constructor as a class method
+  print(alfaZero);
+
+  var alfaFromJson = A.fromJson(json: {'x' : 5, 'y' : 10});
+  print(alfaFromJson);
+
 }
 
