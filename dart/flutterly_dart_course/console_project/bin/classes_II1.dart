9532e189303412f44ed6f050fcf8f8db19828859
mdeyanov83 <mdeyanov@gmail.com>
2025-08-24 23:33:39 +0000

    Sun, Aug 24, 2025, 11:33 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_II1.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_II1.dart
@@ -1,5 +1,10 @@
 //! Classes II // timestamp 5:00:00
 
+//! Inheritance // 5:01:00
+//* Everything you create is an object instantiated from a class
+//* Every class you create will inherit, by default, the OBJECT class
+
+
 
 
 
