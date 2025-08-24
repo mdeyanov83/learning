c0498e858c6a312951f1102c1634c2957b4974d4
mdeyanov83 <mdeyanov@gmail.com>
2025-08-24 23:39:49 +0000

    Sun, Aug 24, 2025, 11:39 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_II1.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_II1.dart
@@ -2,10 +2,26 @@
 
 //! Inheritance // 5:01:00
 //* Everything you create is an object instantiated from a class
-//* Every class you create will inherit, by default, the OBJECT class
+//* Every class you create will inherit, by default from the topmost class, the OBJECT class
+// Every class ClassName extends Object {}, without implicitly defining it
+// keyword extends = means inherits
+
+//! Inheritance - implies sharing of behavior from a class to another one
+//* if one class extends another, all fields/methods from the second will be available in the first
 
 
+class Animal {
+  final String name;
+  // Constructor
+  Animal({required this.name});
 
+  void whatAmI() => print('I\'m an animal');
+}
+
+class Bird extends Animal {
+
+
+}
 
 
 void main(List<String> args) {
