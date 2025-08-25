b7c7e08fc6e455c08f9ce827d924a193e96b6ae2
mdeyanov83 <mdeyanov@gmail.com>
2025-08-25 12:40:41 +0000

    Mon, Aug 25, 2025, 12:40 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_II2.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_II2.dart
@@ -3,12 +3,21 @@
 //! ABSTRACTION
 //* Abstract classes, Abstract methods, Interfaces
 
+//* An interface is a class that contains a list of fields and methods headers
+
+
+abstract class UserRepositoryInterface {
+  void create();
+  void read();
+  void update();
+  void delete();
+}
+
+
+
 class Animal {
   final String name;
-  // Constructor
   Animal({required this.name});
-  // Named constructor
-  // Animal.fromJson() : name = 'Jerry';
 
   void whatAmI() => print('I\'m an animal');
   void chase(Animal a) {}
@@ -18,18 +27,14 @@ class Mouse extends Animal {
   Mouse (): super(name: 'Jerry');
 }
 
-//* Example: covariant override
 class Cat extends Animal {
   Cat() : super(name: 'Tom');
-  @override // override the Animal.chase method to take in a Mouse, instead of Animal
-  // void chase(Mouse m) {} // Error, due to requirement for parameters to stay compatible with e base type
-  void chase(covariant Mouse m) {} // with 'covariant' this rule is relaxed, and we can explicitly override the parameter type
+  @override
+  void chase(covariant Mouse m) {}
 }
 
-//* class hierarchy - Bird extends Animal, Animal extends Object
 class Bird extends Animal {
-  Bird(String name) : super(name: name); //* denotes the superclass default Animal constructor
-  // Bird.fromJson(String name) : super.fromJson(); //* we can call named superclass constructors too
+  Bird(String name) : super(name: name);
 }
 
 class Duck extends Bird {
