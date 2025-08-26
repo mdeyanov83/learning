e33b66c93230c8d725bd07498d20872927ae8038
mdeyanov83 <mdeyanov@gmail.com>
2025-08-26 01:15:49 +0000

    Tue, Aug 26, 2025, 1:15 AM +00:00

---
+++ dart/flutterly_dart_course/console_project/bin/classes_II4.dart
@@ -0,0 +1,26 @@
+//! Classes 2
+
+//! Mixins (continued) // timestamp 5:20:00
+
+
+class Performer {
+  void perform() => print('Performs!');
+}
+
+mixin Guitarist {
+  void playGuitar() => print('Playing the guitar');
+  void perform() => playGuitar();
+}
+
+mixin Drummer {
+  void playDrums() => print('Playing the drums');
+  void perform() => playDrums();
+}
+
+class Musician extends Performer with Drummer, Guitarist{}
+
+
+
+void main() {
+
+}
