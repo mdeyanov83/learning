cc414da8f4e583e93f44ef72de2f1c4b2bccb253
mdeyanov83 <mdeyanov@gmail.com>
2025-08-26 12:42:53 +0000

    Tue, Aug 26, 2025, 12:42 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_II6.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_II6.dart
@@ -1,19 +1,26 @@
 /*
 ! Classes
 ! Extension Methods - timestamp 5:28:35
-
-
-
-
-
+* Extension methods - a way to add functionality to existing libraries and classes
+* You can create extension methods on any class
 */
 
+// class IntegerExtension extends int{ //! Error - int class cannot be extended
+//   int get luckyNumber => 12;
+// }
+//? int.luckyNumber - this is what we want to have
 
-class IntegerExtension extends int{
-
+extension IntegerExtension on int{
+  int get luckyInteger => 12;
+  int add15() => this + 15;
 }
 
-
 void main() {
+  int intNumber = 5;
 
+  // 5.luckyNumber; // int object - 5
+  print('5.luckyInteger --> ${5.luckyInteger}');
+
+  // 10.add15();
+  print('10.add15() --> ${10.add15()}');
 }
