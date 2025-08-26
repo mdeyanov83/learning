bba90acf90b8a891f9d113c2108560a71d8534ec
mdeyanov83 <mdeyanov@gmail.com>
2025-08-26 11:23:54 +0000

    Tue, Aug 26, 2025, 11:23 AM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_II4.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_II4.dart
@@ -1,17 +1,29 @@
-//! Classes 2
+/*
+! Classes 2
+! Mixins (continued) // timestamp 5:20:00
 
-//! Mixins (continued) // timestamp 5:20:00
-//? How class hierarchy work?
-//* Mixins are applied left -> right. Rightmost mixin wins if there are conflicting methods
-//? How hierarchy is built?
-//* class A with M1, M2 {}
-// is equivalent to
-//* class A extends Object with M1 {}
-//* class _A_M1 extends A with M2 {}
-// so the class chain looks like:
-// Object -> A+M1 -> A+M1+M2   // M2 overrides methods from M1
+? How class hierarchy work?
+* Mixins are applied left -> right. Rightmost mixin wins if there are conflicting methods
+? How hierarchy is built?
+* class A with M1, M2 {}
+is equivalent to
+* class A extends Object with M1 {}
+* class _A_M1 extends A with M2 {}
+so the class chain looks like:
+* Object -> A+M1 -> A+M1+M2   // M2 overrides methods from M1
+? Adding a superclass?
+class Base {
+  void hello() => print("Hello from Base");
+}
+class A extends Base with M1, M2 {}
+* The resolution order is: Object -> Base -> A+M1 -> A+M1+M2
+* Method lookup checks in the following order:
+* M2 -> M1 -> Base -> Object
+
+In the following example musician.perform(); will call the Guitarist method
 
 
+*/
 
 
 
