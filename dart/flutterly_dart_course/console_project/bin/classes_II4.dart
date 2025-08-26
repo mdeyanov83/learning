210624e0096e018f5cfe6ffcf1f07f9b2eccc9dc
mdeyanov83 <mdeyanov@gmail.com>
2025-08-26 11:17:19 +0000

    Tue, Aug 26, 2025, 11:17 AM +00:00

--- dart/flutterly_dart_course/console_project/bin/classes_II4.dart
+++ dart/flutterly_dart_course/console_project/bin/classes_II4.dart
@@ -1,8 +1,18 @@
 //! Classes 2
 
 //! Mixins (continued) // timestamp 5:20:00
-//* How class hierarchy work?
+//? How class hierarchy work?
 //* Mixins are applied left -> right. Rightmost mixin wins if there are conflicting methods
+//? How hierarchy is built?
+//* class A with M1, M2 {}
+// is equivalent to
+//* class A extends Object with M1 {}
+//* class _A_M1 extends A with M2 {}
+// so the class chain looks like:
+// Object -> A+M1 -> A+M1+M2   // M2 overrides methods from M1
+
+
+
 
 
 class Performer {
