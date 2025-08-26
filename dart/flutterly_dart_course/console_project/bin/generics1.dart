10ea8607fa958dab036bf0d27d8a64086e796fc5
mdeyanov83 <mdeyanov@gmail.com>
2025-08-26 23:17:18 +0000

    Tue, Aug 26, 2025, 11:17 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/generics1.dart
+++ dart/flutterly_dart_course/console_project/bin/generics1.dart
@@ -4,6 +4,14 @@
 *1. What is the purpose of using generics?
 *2. How can we use them to enhance the quality of our code?
 
+! Naming convention - most often single capital letter
+? collections
+* abstract class List<E>    - <E> = elements
+* abstract class Set<E>     - <E> = elements
+* abstract class HashSet<E> - <E> = elements
+? maps
+* abstract class Map<K, V> - <K, V> = keys and values
+
 
 
 
@@ -14,5 +22,5 @@
  List<int> list = [1, 2, 3];
 
 void main(List<String> args) {
-  
+
 }
