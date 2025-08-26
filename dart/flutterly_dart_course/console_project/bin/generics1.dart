373cf8a63274bf5ac1de11f3c8eb182fd0503a50
mdeyanov83 <mdeyanov@gmail.com>
2025-08-26 14:23:16 +0000

    Tue, Aug 26, 2025, 2:23 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/generics1.dart
+++ dart/flutterly_dart_course/console_project/bin/generics1.dart
@@ -1,6 +1,8 @@
 /*
 ! Generics - timestamp 5:31:00
-* Generic types
+* Generic types - <K, V> - this <> angle bracket declaration marks the class or method as being generic
+*1. What is the purpose of using generics?
+*2. How can we use them to enhance the quality of our code?
 
 
 
