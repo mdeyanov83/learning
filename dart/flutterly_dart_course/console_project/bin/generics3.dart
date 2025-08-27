3b4d6ce3ec1804b02278fda07aadd6afcbd32645
mdeyanov83 <mdeyanov@gmail.com>
2025-08-27 01:21:46 +0000

    Wed, Aug 27, 2025, 1:21 AM +00:00

--- dart/flutterly_dart_course/console_project/bin/generics3.dart
+++ dart/flutterly_dart_course/console_project/bin/generics3.dart
@@ -18,10 +18,11 @@ class Stack<T> {
   T get peak => _stack.last;
   int get length => _stack.length;
 
-  // getter
-  bool canPop => _stack.isNotEmpty;
+  // getter - better implementation, as it looks like a property when called stack.canPop
+  bool get canPop => _stack.isNotEmpty;
+
   // method to check if we can pop an element or not
-  bool canPop() => _stack.isNotEmpty;
+  // bool canPop() => _stack.isNotEmpty;
 
   // pop method
   T pop() {
