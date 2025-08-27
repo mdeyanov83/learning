ec38d07f698cb37b162fa47cf74f2cecd13d3e18
mdeyanov83 <mdeyanov@gmail.com>
2025-08-27 22:13:52 +0000

    Wed, Aug 27, 2025, 10:13 PM +00:00

---
+++ dart/flutterly_dart_course/console_project/libraries2.dart
@@ -0,0 +1,17 @@
+/*
+! Libraries - timestamp 6:05:10
+
+Every .dart file is a library
+
+? Can one library be formed out of more than one file?
+* Yes - a library can be formed by one or more files.
+* In case - multiple files - then each file is no longer a separate library, but rather represent the same library
+
+
+? Can one library contain multiple libraries?
+
+
+
+
+
+*/
