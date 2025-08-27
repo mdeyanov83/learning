eb17168f8af8c558583b8e2d7e34a01ab22bd8cb
mdeyanov83 <mdeyanov@gmail.com>
2025-08-27 20:15:43 +0000

    Wed, Aug 27, 2025, 8:15 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/libraries1.dart
+++ dart/flutterly_dart_course/console_project/bin/libraries1.dart
@@ -1,7 +1,11 @@
 /*
 ! Libraries & Private fields - timestamp 5:57:00
 
+? What these libraries are?
+? How they really work?
+? Where they actually live?
 
+* How private fields work in conjunction with libraries
 
 
 */
