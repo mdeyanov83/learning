0dcf56786090da3afdedf51a662be3c44595628b
mdeyanov83 <mdeyanov@gmail.com>
2025-08-27 18:59:33 +0000

    Wed, Aug 27, 2025, 6:59 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/generics4.dart
+++ dart/flutterly_dart_course/console_project/bin/generics4.dart
@@ -5,6 +5,27 @@
 */
 
 
+class Utils<T>{
+  // static int? getItem(List<int> list, int index) =>
+  //   list.asMap().containsKey(index) ? list[index] : null;
+
+  //! In oder for the method to work with other types than int (String for ex.) we need
+  //! to either create another method accepting String
+  // static String? getItem(List<String> list, int index) =>
+  //   list.asMap().containsKey(index) ? list[index] : null;
+
+  //! or make a generic method
+  static T? getItem(List<int> list, int index) =>
+    list.asMap().containsKey(index) ? list[index] : null;
+
+}
+
+
 void main(List<String> args) {
-  
+  // var list = [1, 2, 3, 4];
+  var list = ['a', 'b'];
+
+  print(Utils.getItem(list, 1));
+
+
 }
