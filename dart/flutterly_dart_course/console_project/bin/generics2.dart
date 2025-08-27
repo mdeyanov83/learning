1796185d6c7b337c9bce510b18ad4cf13c5a4ed0
mdeyanov83 <mdeyanov@gmail.com>
2025-08-27 01:18:25 +0000

    Wed, Aug 27, 2025, 1:18 AM +00:00

--- dart/flutterly_dart_course/console_project/bin/generics2.dart
+++ dart/flutterly_dart_course/console_project/bin/generics2.dart
@@ -2,7 +2,6 @@
 ! Generics - timestamp 5:46:05
 Even though you don't have to mention the <> types all the time
 It is recommended to always mention them, as it will make life so much easier during debugging
-
 */
 
 class RandomClass<E> {}
---
+++ dart/flutterly_dart_course/console_project/bin/generics3.dart
@@ -0,0 +1,47 @@
+/*
+! Generics - timestamp 5:47:00
+
+! Data Structures - Stack implementation
+Stack is a LIFO data structure - Last In - First Out
+
+
+*/
+
+
+
+
+class Stack<T> {
+  // _stack - private
+  final List<T> _stack = [];
+
+  // getters - returning the peak(last element) and the length of the stack
+  T get peak => _stack.last;
+  int get length => _stack.length;
+
+  // method to check if we can pop an element or not
+  bool get canPop => _stack.isNotEmpty;
+
+  // pop method
+  T pop() {
+    final T last = _stack.last;
+    _stack.removeLast();
+    return last;
+  }
+
+  // push method
+  void push(T value) => _stack.add(value);
+}
+
+
+
+void main(List<String> args) {
+
+  var stackInt = Stack<int>();
+  var stackString = Stack<String>();
+
+  stackInt.push(2);
+  stackInt.push(3);
+  stackInt.push(4);
+  print(stackInt.pop()); // should print 4 and pop the 4 out of the stack
+  print(stackInt.peak); // should print 3
+}
