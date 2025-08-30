561d8645607c64ff45e137ffe0bcd166be714387
mdeyanov83 <mdeyanov@gmail.com>
2025-08-30 20:49:13 +0000

    Sat, Aug 30, 2025, 8:49 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/isolates1.dart
+++ dart/flutterly_dart_course/console_project/bin/isolates1.dart
@@ -2,11 +2,6 @@
 ! Introduction to Synchronous and Asynchronous workflow
 
 ! Isolates
-06:41:00 - (20) Dart Isolates Explained (Sync & Async Chapter)
-06:50:11 - (20.1) Dart Isolate Structure, Event Queue, Event Loop, Single Thread, Memory
-06:57:50 - (20.2) Parallelism
-!06:59:00 - (20.3) Isolate Groups
-07:00:41 - (20.4) Microtask queue
 
 * Isolate - Independent unit of execution in Dart
   - Isolated memory spaces: Each isolate has its own memory heap
