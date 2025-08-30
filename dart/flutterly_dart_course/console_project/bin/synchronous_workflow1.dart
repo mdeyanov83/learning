9d38243cf5f1895aac153d1bf4bb792ccf59e243
mdeyanov83 <mdeyanov@gmail.com>
2025-08-30 21:20:00 +0000

    Sat, Aug 30, 2025, 9:20 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/synchronous_workflow1.dart
+++ dart/flutterly_dart_course/console_project/bin/synchronous_workflow1.dart
@@ -1,9 +1,19 @@
 /*
 ! Synchronous Workflow
 
+07:05:00 - (21) Dart Synchronous Workflows
+07:09:01 - (21.1) Iterables and Lists
+07:20:00
+
+* Synchronous operation - a task that needs to be completed before moving on to the next one
+
+
+
+
+
 
 */
 
 void main(List<String> args) {
-  
+
 }
