406983604de191b632f4ca6f63fff81b772ed65b
mdeyanov83 <mdeyanov@gmail.com>
2025-08-30 22:35:04 +0000

    Sat, Aug 30, 2025, 10:35 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/synchronous_workflow1.dart
+++ dart/flutterly_dart_course/console_project/bin/synchronous_workflow1.dart
@@ -6,8 +6,14 @@
 07:20:00
 
 * Synchronous operation - a task that needs to be completed before moving on to the next one
+  A task or function in a synchronous operation usually returns a result (a value, a type, class, etc.)
+  When we want the result to consist of 0 or multiple values --> return an Iterable collection of values
 
-             |  1 value  | 0 or more values
+* Iterable - an abstract collection of values
+  - Lazily constructed - it will generate its items only when an element is accessed
+  - Traversed with the help of an iterator(curr, nextItem())
+
+------------ |  1 value  | 0 or more values
 ------------------------------------------
 synchronous  |     T     |   ITERABLE<T> |
 ------------------------------------------
