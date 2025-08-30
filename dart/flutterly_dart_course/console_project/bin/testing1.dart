568dedf640eb824128f64a03ec1e139e094eedd4
mdeyanov83 <mdeyanov@gmail.com>
2025-08-30 12:47:34 +0000

    Sat, Aug 30, 2025, 12:47 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/testing1.dart
+++ dart/flutterly_dart_course/console_project/bin/testing1.dart
@@ -2,10 +2,20 @@
 ! Testing - timestamp 6:19:30
 ! package: dart_testing
 
+When writing test, you must think of every possible scenario where the app or function
+will work, will NOT work and may crash.
+
 Test types
-  Unit
-  Component
-  E2E
+  - Unit - test the smallest portion of code - a function, method or a class.
+  - Component ("Widget") - Verify that a component (which usually consists
+    of multiple classes) behaves as expected.
+    A component test, often requires the use of Mock Objects (objects that can
+    mimic user actions, events, instance child components, etc.)
+  - E2E - Integration "E2E" (End-To-End) tests - Verify the behavior of the
+    entire app, or a large chunk of an app. An integration test, generally runs
+    on a simulated or real device and consists of 2 pieces:
+    > the app itself
+    > the test app that will put the app through its paces.
 
 
 
