9397a2a8cd1b527cd0c6cc4e4b417175adb06ec3
mdeyanov83 <mdeyanov@gmail.com>
2025-08-31 13:49:57 +0000

    Sun, Aug 31, 2025, 1:49 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/asynchronous_workflow1.dart
+++ dart/flutterly_dart_course/console_project/bin/asynchronous_workflow1.dart
@@ -11,7 +11,7 @@ asynchronous | Future<T> |  STREAM<T>   |
 Future<T> - A single value, that will be calculated and retrieved later
 Stream<T> - emit multiple values Asynchronously, while other tasks are executed in the background
 
-! FUTURES
+! FUTURES - timestamp 7:22:50
 (Single async result)
 * States:
   - Unprocessed - not yet processed by the event loop
@@ -24,13 +24,27 @@ Stream<T> - emit multiple values Asynchronously, while other tasks are executed 
 
 
 
-07:20:00 - (22) Dart Asynchronous Workflows
 07:22:51 - (22.1) Futures in-depth
 07:50:31 - (22.2) Streams in-depth
 
 */
 
 
+import 'dart:async';
+
 void main(List<String> args) {
+  print('Start');
+
+  Future(() => 1).then(print);
+  Future(() => Future(() => 2)).then(print);
+
+  Future.delayed(const Duration(seconds: 1), () => 3).then(print);
+  Future.delayed(const Duration(seconds: 1), () => Future(() => 4)).then(print);
+
+  Future.value(5).then(print);
+  Future.value(Future(() => 6)).then(print);
+
+  
+
 
 }
