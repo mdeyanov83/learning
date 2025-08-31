34416bb6649df887183c0f0a42681c9657c43144
mdeyanov83 <mdeyanov@gmail.com>
2025-08-31 12:58:19 +0000

    Sun, Aug 31, 2025, 12:58 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/asynchronous_workflow1.dart
+++ dart/flutterly_dart_course/console_project/bin/asynchronous_workflow1.dart
@@ -1,7 +1,17 @@
 /*
 ! Asynchronous Workflow - timestamp - 7:20:00
+? What is an Asynchronous operation - A task that that needs to be solved
+? But doesn't need to wait before proceeding to the next one
 
 
+
+
+
+
+
+07:20:00 - (22) Dart Asynchronous Workflows
+07:22:51 - (22.1) Futures in-depth
+07:50:31 - (22.2) Streams in-depth
 
 */
 
