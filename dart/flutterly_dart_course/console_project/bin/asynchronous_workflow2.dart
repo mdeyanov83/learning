6fbefe816f9e5fcca6915a718a9e805336133503
mdeyanov83 <mdeyanov@gmail.com>
2025-09-01 00:46:36 +0000

    Mon, Sep 1, 2025, 12:46 AM +00:00

--- dart/flutterly_dart_course/console_project/bin/asynchronous_workflow2.dart
+++ dart/flutterly_dart_course/console_project/bin/asynchronous_workflow2.dart
@@ -7,9 +7,16 @@
 
 07:22:51 - (22.1) Futures in-depth
 07:50:31 - (22.2) Streams in-depth
+Current - 7:39:35
 */
 
 
+import 'dart:async';
+
 void main(List<String> args) {
+
+  print('1');
+  scheduleMicrotask(() => print('2'));
+
 
 }
