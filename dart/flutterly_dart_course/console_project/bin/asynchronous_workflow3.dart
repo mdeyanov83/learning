f406e957ea12fcafc061d0ae10f730dc010bd8a9
mdeyanov83 <mdeyanov@gmail.com>
2025-09-01 20:01:44 +0000

    Mon, Sep 1, 2025, 8:01 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/asynchronous_workflow3.dart
+++ dart/flutterly_dart_course/console_project/bin/asynchronous_workflow3.dart
@@ -11,10 +11,6 @@
 * 'await' can be used only in an 'async' function
   Syntax: Type functionName(args) async {body}
 
-? await is syntactic sugar for
-? “pause this async function until the future completes,
-? then continue execution in a microtask”.
-
 ! Await Mechanics
 1. You call an "async" function -> it immediately returns a Future (even before hitting await)
 2. When execution reaches an await someFuture; :
@@ -25,7 +21,17 @@
     -> When the future completes, a continuation (the "rest of the async function")
       is scheduled in the microtask queue
 
+* Always returns a future: An async function itself returns a Future<T>
+* Suspension point: Code after await is put on hold and resumes later.
+* Resumption uses microtasks: When a future completes, await resumes the
+  * async function in the microtask queue, ensuring the continuation
+  * happens before the next event task.
+* Error propagation: if the awaited future completes with an error,
+  * that error is re-thrown at the await point (so you can catch it with try/catch)
 
+? await is syntactic sugar for
+? “pause this async function until the future completes,
+? then continue execution in a microtask”.
 
 
 
@@ -56,7 +62,6 @@ void main(List<String> args) async {
   a = await Future(() => 1);
 
   // print('Print(a) -> $a');
-
 
   print('End');
 }
