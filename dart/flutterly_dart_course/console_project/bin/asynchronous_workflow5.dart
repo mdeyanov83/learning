d6811b86095fa25065e4670b6177df2e7395e485
mdeyanov83 <mdeyanov@gmail.com>
2025-09-01 23:19:40 +0000

    Mon, Sep 1, 2025, 11:19 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/asynchronous_workflow5.dart
+++ dart/flutterly_dart_course/console_project/bin/asynchronous_workflow5.dart
@@ -10,12 +10,15 @@ current 7:54:00
 import 'dart:async';
 void main(List<String> args) {
 
-  // Initialize the StreamController
-  final StreamController streamController = StreamController<int>();
+  // Initialize the StreamController as a broadcast stream (multiple listeners supported)
+  final StreamController streamController = StreamController<int>.broadcast();
 
   // Initialize the StreamSubscription (.listen) and print received values
-  // streamController.stream.listen((value) => print('Data: $value++'));
+  // streamController.stream.listen((value) => print('Data: $value'));
   final streamSubscription = streamController.stream.listen(print); // same as above line
+
+  // Initialize a second StreamSubscription
+  final otherStreamSubscription = streamController.stream.listen((value) => print('Second *2: ${value*2}'));
 
   var value = 0;
   // Adding values to the stream
