f181cf3283668f199ca90bd505175511913f88c3
mdeyanov83 <mdeyanov@gmail.com>
2025-09-01 20:16:01 +0000

    Mon, Sep 1, 2025, 8:16 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/asynchronous_workflow3.dart
+++ dart/flutterly_dart_course/console_project/bin/asynchronous_workflow3.dart
@@ -33,12 +33,7 @@
 ? “pause this async function until the future completes,
 ? then continue execution in a microtask”.
 
-
-
-07:22:51 - (22.1) Futures in-depth
-07:50:31 - (22.2) Streams in-depth
-Current - 7:44:30
-7:48:55
+* await == .then(...) under the hood
 */
 
 import 'dart:async';
