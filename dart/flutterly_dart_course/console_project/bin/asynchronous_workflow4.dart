65834725bf70319f4ec8ed4a3a0238701a6cb3d1
mdeyanov83 <mdeyanov@gmail.com>
2025-09-01 20:24:57 +0000

    Mon, Sep 1, 2025, 8:24 PM +00:00

--- dart/flutterly_dart_course/console_project/bin/asynchronous_workflow4.dart
+++ dart/flutterly_dart_course/console_project/bin/asynchronous_workflow4.dart
@@ -7,7 +7,7 @@ Streams can return 0, 1 or multiple values in the future.
 The difference between Stream and Iterable is that
 stream values are returned asynchronously one by one in the future
 
-
+* .listen -> similar to .then on Futures
 
 
 
@@ -18,7 +18,11 @@ stream values are returned asynchronously one by one in the future
 import 'dart:async';
 void main(List<String> args) {
 
+  print('start');
+  
   //? Emits values from 0 to infinite with 1 second delay each
   Stream.periodic(const Duration(seconds: 1), (x) => x) //? 0 1 2 3 4 5 ...
     .listen(print);
+
+  print('end');
 }
