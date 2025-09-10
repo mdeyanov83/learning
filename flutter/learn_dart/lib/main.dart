576d58bf844fa8f0cb0a6be4820a7a700e9eede2
mdeyanov83 <mdeyanov@gmail.com>
2025-09-10 22:29:39 +0000

    Wed, Sep 10, 2025, 10:29 PM +00:00

--- flutter/learn_dart/lib/main.dart
+++ flutter/learn_dart/lib/main.dart
@@ -9,9 +9,6 @@ void test() {
 
   List<String?>? names = ['Foo', 'Bar', null];
   names = null;
-  print("test");
-  print("test");
-  print("test2");
 }
 
 
@@ -44,7 +41,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page 3'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
