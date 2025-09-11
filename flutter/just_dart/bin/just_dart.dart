75cee1500b05bf16c0be90c1772e8217a9da9203
mdeyanov83 <mdeyanov@gmail.com>
2025-09-11 22:14:05 +0000

    Thu, Sep 11, 2025, 10:14 PM +00:00

--- flutter/just_dart/bin/just_dart.dart
+++ flutter/just_dart/bin/just_dart.dart
@@ -1,28 +1,15 @@
 
-enum AnimalType { cat, dog, bunny }
+Function
 
 
 void test(AnimalType animalType) {
 
-  switch(animalType) {
-    case AnimalType.bunny:
-      print("bunny");
-      break;
-    case AnimalType.cat:
-      print("Cat");
-    case AnimalType.dog:
-      print("Dog");
-
-  }
-
-
-  print("Function is finised");
 
 }
 
 
 void main(List<String> arguments) {
 
-  test( );
+  test();
 
 }
