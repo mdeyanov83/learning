d06c1e726e994e504d8fb8158b6c49f4e786e29b
mdeyanov83 <mdeyanov@gmail.com>
2025-09-11 19:59:07 +0000

    Thu, Sep 11, 2025, 7:59 PM +00:00

--- flutter/just_dart/bin/just_dart.dart
+++ flutter/just_dart/bin/just_dart.dart
@@ -1,6 +1,12 @@
 import 'package:just_dart/just_dart.dart' as just_dart;
 
+
+void test() {
+
+
+}
+
+
 void main(List<String> arguments) {
-  print('Hello world: ${just_dart.calculate()}!');
-  print('Second Hello');
+  test();
 }
