6d50e884e1d796d282fb114533c2a2960ec5128c
mdeyanov83 <mdeyanov@gmail.com>
2025-09-12 13:33:01 +0000

    Fri, Sep 12, 2025, 1:33 PM +00:00

--- flutter/just_dart/bin/just_dart.dart
+++ flutter/just_dart/bin/just_dart.dart
@@ -5,9 +5,10 @@ Stream<String> getName() {
 }
 
 void test() async {
-  await for (final value in getName());
-  print(value);
-
+  await for (final value in getName()) {
+    print(value);
+  }
+  print('Stream finished working');
 }
 
 void main(List<String> arguments) {
