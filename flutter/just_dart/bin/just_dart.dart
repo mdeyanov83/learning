dfa0f4d8e3fe5971245afab849be3fe8d89a8b4b
mdeyanov83 <mdeyanov@gmail.com>
2025-09-12 16:22:01 +0000

    Fri, Sep 12, 2025, 4:22 PM +00:00

--- flutter/just_dart/bin/just_dart.dart
+++ flutter/just_dart/bin/just_dart.dart
@@ -1,17 +1,21 @@
 
-Iterable<int> getOneTwoThree() sync* {
-  for (int i = 0; i < 10; i++) {
-    yield i;
-  }
+
+class PairOfStrings {
+  final String value1;
+  final String value2;
+  PairOfStrings(this.value1, this.value2);
 
 }
 
+class PairOfIntegers {
+  final int value1;
+  final int value2;
+  PairOfIntegers(this.value1, this.value2);
+
+}
 
 void test() async {
-  for (final value in getOneTwoThree()) {
-    if (value == 5) break;
-    print(value);
-  }
+
 }
 
 void main(List<String> arguments) {
