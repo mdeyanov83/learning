10c9092d9e1efee15ca9cad011ce6ba5507b1949
mdeyanov83 <mdeyanov@gmail.com>
2025-09-12 13:42:29 +0000

    Fri, Sep 12, 2025, 1:42 PM +00:00

--- flutter/just_dart/bin/just_dart.dart
+++ flutter/just_dart/bin/just_dart.dart
@@ -1,11 +1,16 @@
 
-Iterable<int> getOneTwoThree() {
-  return [1, 2, 3];
+Iterable<int> getOneTwoThree() sync* {
+  for (int i = 0; i < 10; i++) {
+    yield i;
+  }
+
 }
 
 
 void test() async {
-
+  for (final value in getOneTwoThree()) {
+    print(value);
+  }
 }
 
 void main(List<String> arguments) {
