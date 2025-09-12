4d1427db94dbae3547cec5a94224d30389834097
mdeyanov83 <mdeyanov@gmail.com>
2025-09-12 12:59:07 +0000

    Fri, Sep 12, 2025, 12:59 PM +00:00

--- flutter/just_dart/bin/just_dart.dart
+++ flutter/just_dart/bin/just_dart.dart
@@ -1,35 +1,15 @@
 
+int multipliedByTwo(int a) => a * 2;
 
 
-class Cat{
-  final String name;
-  Cat(this.name);
-}
-
-class Person {
-  final String firstName;
-  final String lastName;
-  Person(this.firstName, this.lastName);
-}
-
-extension FullName on Person {
-  String get fullName => '$firstName $lastName';
-}
-
-extension Run on Cat {
-  void run() {
-    print('Cat $name is running');
-  }
+Future<int> heavyFutureThatMultipliesByTwo(int a) {
+  Future.delayed(Duration(seconds: 3), (e) { rturn a }
 }
 
 
 void test() {
-  final meow = Cat('Fluffers');
-  print(meow.name);
-  meow.run();
 
-  final foo = Person('Foo', 'Bar');
-  print(foo.fullName);
+
 }
 
 void main(List<String> arguments) {
