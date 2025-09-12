aa32742fb6a24fb1a57648751419202355d30d91
mdeyanov83 <mdeyanov@gmail.com>
2025-09-12 11:13:48 +0000

    Fri, Sep 12, 2025, 11:13 AM +00:00

--- flutter/just_dart/bin/just_dart.dart
+++ flutter/just_dart/bin/just_dart.dart
@@ -8,13 +8,16 @@ class Cat{
 
 
 extension Run on Cat {
-  
+  void run() {
+    print('Cat $name is running');
+  }
 }
 
 
 void test() {
   final meow = Cat('Fluffers');
   print(meow.name);
+  meow.run();
 }
 
 void main(List<String> arguments) {
