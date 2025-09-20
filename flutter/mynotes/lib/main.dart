5ef9ee08a957bd021e44e857f90fcae9d6101655
mdeyanov83 <mdeyanov@gmail.com>
2025-09-20 11:14:41 +0000

    Sat, Sep 20, 2025, 11:14 AM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -6,11 +6,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    print('Building');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
