49ee51fc3d93c1d06bd48621d9b300b4c750e07f
mdeyanov83 <mdeyanov@gmail.com>
2025-09-20 22:12:12 +0000

    Sat, Sep 20, 2025, 10:12 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
