8882a90360902fc1e3409f151fb7ca452a167310
mdeyanov83 <mdeyanov@gmail.com>
2025-09-20 13:40:13 +0000

    Sat, Sep 20, 2025, 1:40 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -22,6 +22,10 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Register'),
       ),
+      body: TextButton(onPressed: () {
+
+      },
+      child: child),
     );
   }
 }
