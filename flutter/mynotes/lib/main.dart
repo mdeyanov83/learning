97e24b88a273314cd313e5c57330ef3cb643c2b2
mdeyanov83 <mdeyanov@gmail.com>
2025-09-21 22:48:02 +0000

    Sun, Sep 21, 2025, 10:48 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -30,7 +30,7 @@ class HomePage extends StatelessWidget {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
               final user = FirebaseAuth.instance.currentUser;
-              if (user.emailVerified)
+              if (user.isEmailVerified)
               return Text('Done');
             default:
               return const Text('Loading...');
