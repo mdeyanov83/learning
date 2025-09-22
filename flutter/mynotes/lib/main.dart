2372568f4f1b0e0a48da48e9f8be16d964641b0d
mdeyanov83 <mdeyanov@gmail.com>
2025-09-22 14:21:56 +0000

    Mon, Sep 22, 2025, 2:21 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -58,20 +58,17 @@ class VerifyEmailView extends StatefulWidget {
 class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Verify email')),
-      body: Column(
-        children: [
-          const Text('Please verify your email address:'),
-          TextButton(
-            onPressed: () async {
-              final user = FirebaseAuth.instance.currentUser;
-              await user?.sendEmailVerification();
-            },
-            child: const Text('Send email verification'),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        const Text('Please verify your email address:'),
+        TextButton(
+          onPressed: () async {
+            final user = FirebaseAuth.instance.currentUser;
+            await user?.sendEmailVerification();
+          },
+          child: const Text('Send email verification'),
+        ),
+      ],
     );
   }
 }
