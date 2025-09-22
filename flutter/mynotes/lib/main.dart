491f6dddf779c17cdf11c2c3c4d242985bd93f45
mdeyanov83 <mdeyanov@gmail.com>
2025-09-22 23:13:19 +0000

    Mon, Sep 22, 2025, 11:13 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -54,3 +54,22 @@ class HomePage extends StatelessWidget {
 }
 
 
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        backgroundColor: Colors.blue),
+      ),
+      body:
+    ) ;
+  }
+}
