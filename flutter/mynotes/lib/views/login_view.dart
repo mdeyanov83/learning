3c7ef4aec0e5c0f82c85a54d88a96a435bbe9823
mdeyanov83 <mdeyanov@gmail.com>
2025-09-22 22:30:42 +0000

    Mon, Sep 22, 2025, 10:30 PM +00:00

--- flutter/mynotes/lib/views/login_view.dart
+++ flutter/mynotes/lib/views/login_view.dart
@@ -70,7 +70,7 @@ class _LoginViewState extends State<LoginView> {
             Navigator.of(context).pushNamedAndRemoveUntil(
               '/register/',
               (route) => false
-            )
+            );
           },
           child: const Text('Not registered yet? Register here!'),
         ),
