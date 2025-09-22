5f6b6dc08435a90fbf8ce04eb1ba90aa7de3bf04
mdeyanov83 <mdeyanov@gmail.com>
2025-09-22 12:38:27 +0000

    Mon, Sep 22, 2025, 12:38 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -10,7 +11,7 @@ void main() {
     MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+      home: const RegisterView(),
     ),
   );
 }
