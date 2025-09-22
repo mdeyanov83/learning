84a597aeb20b6a528e63f2d446e410f35aa70911
mdeyanov83 <mdeyanov@gmail.com>
2025-09-22 22:29:56 +0000

    Mon, Sep 22, 2025, 10:29 PM +00:00

--- flutter/mynotes/lib/views/login_view.dart
+++ flutter/mynotes/lib/views/login_view.dart
@@ -67,7 +67,10 @@ class _LoginViewState extends State<LoginView> {
         ),
         TextButton(
           onPressed: () {
-            Navigator.of(context).pushNamedAndRemoveUntil('/register/', predicate)
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              '/register/',
+              (route) => false
+            )
           },
           child: const Text('Not registered yet? Register here!'),
         ),
