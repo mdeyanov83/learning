a5680cf4b96ddf6c08b453cc6450ca9f17c2dcb5
mdeyanov83 <mdeyanov@gmail.com>
2025-09-23 12:57:51 +0000

    Tue, Sep 23, 2025, 12:57 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -98,8 +98,18 @@ Future<bool> showLogoutDialog(BuildContext context) {
         title: const Text('Sign out'),
         content: const Text('Are you sure you want to sign out?'),
         actions: [
-          TextButton(onPressed: () {}, child: const Text('Cancel')),
-          TextButton(onPressed: () {}, child: const Text('Log out')),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Log out'),
+          ),
         ],
       );
     },
