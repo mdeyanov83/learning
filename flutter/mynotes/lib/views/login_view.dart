257768ef4f14d2795f53d60e6c45c404af2a8a04
mdeyanov83 <mdeyanov@gmail.com>
2025-09-23 22:56:12 +0000

    Tue, Sep 23, 2025, 10:56 PM +00:00

--- flutter/mynotes/lib/views/login_view.dart
+++ flutter/mynotes/lib/views/login_view.dart
@@ -96,27 +96,16 @@ class _LoginViewState extends State<LoginView> {
   }
 }
 
-Future<void> showErrorDialog(
-  BuildContext context,
-  String text
-) {
-
+Future<void> showErrorDialog(BuildContext context, String text) {
   return showDialog(
     context: context,
     builder: (context) {
       return AlertDialog(
         title: const Text('An error occurred'),
         content: Text(text),
-
         actions: [
-          TextButton(
-            onPressed:() {},
-            child: const Text('OK'),
-          ),
-        ],
-
-        }
-    )
-
+          TextButton(onPressed: () {}, child: const Text('OK'))],
+      );
+    },
+  );
 }
-
