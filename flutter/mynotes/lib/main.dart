7978a152ab78fe5f7b9de5bcbb9934edb922d8b7
mdeyanov83 <mdeyanov@gmail.com>
2025-09-23 13:33:46 +0000

    Tue, Sep 23, 2025, 1:33 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -17,6 +17,7 @@ void main() {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/notes/': (context) => const NotesView(),
       },
     ),
   );
