7dfc8534b212ad085028b32810770c991d4666f8
mdeyanov83 <mdeyanov@gmail.com>
2025-09-23 22:15:06 +0000

    Tue, Sep 23, 2025, 10:15 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
@@ -16,8 +17,8 @@ void main() {
       home: const HomePage(),
       routes: {
         loginRoute: (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/': (context) => const NotesView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
       },
     ),
   );
