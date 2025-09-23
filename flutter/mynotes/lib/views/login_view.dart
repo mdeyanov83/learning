c02d220160312168e190e7f117a67b715bd94ad8
mdeyanov83 <mdeyanov@gmail.com>
2025-09-23 22:48:11 +0000

    Tue, Sep 23, 2025, 10:48 PM +00:00

--- flutter/mynotes/lib/views/login_view.dart
+++ flutter/mynotes/lib/views/login_view.dart
@@ -1,3 +1,6 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
+import 'dart:nativewrappers/_internal/vm/lib/mirrors_patch.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
@@ -96,6 +99,22 @@ class _LoginViewState extends State<LoginView> {
 Future<void> showErrorDialog(BuildContext context, String text) {
 
 
-  return showDialog(context: context, builder);
+  return showDialog(context: context, builder: (context)) {
+
+    return AlertDialog(
+      title: const Text('An error occurred'),
+      content: Text(text),
+
+      actions: [
+        TextButton(
+          onPressed:() {},
+          child: const Text('OK')
+        ),
+
+      ]
+
+
+    )
+  }
 
 }
