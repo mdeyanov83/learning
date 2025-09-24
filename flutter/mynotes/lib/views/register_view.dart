e7ae6aaf34e5ee62bb2ef20e7e7903fcf8e63469
mdeyanov83 <mdeyanov@gmail.com>
2025-09-24 23:05:51 +0000

    Wed, Sep 24, 2025, 11:05 PM +00:00

--- flutter/mynotes/lib/views/register_view.dart
+++ flutter/mynotes/lib/views/register_view.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
@@ -83,10 +84,26 @@ class _RegisterViewState extends State<RegisterView> {
                   'This is an invalid email address',
                 );
               } on GenericAuthException {
-                await showErrorDialog(
-                  context,
-                  'Failed to register',
-                );
+                await showErrorDialog(context, 'Failed to register',)
+              }
+
+
+
+              on FirebaseAuthException catch (e) {
+                if (e.code == 'weak-password') {
+                  await showErrorDialog(context, 'Weak password');
+                } else if (e.code == 'email-already-in-use') {
+                  await showErrorDialog(context, 'Email is already in use');
+                } else if (e.code == 'invalid-email') {
+                  await showErrorDialog(
+                    context,
+                    'This is an invalid email address',
+                  );
+                } else {
+                  await showErrorDialog(context, 'Error: ${e.code}');
+                }
+              } catch (e) {
+                await showErrorDialog(context, e.toString());
               }
             },
             child: const Text('Register'),
