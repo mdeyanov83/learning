a78cb5b4d117004934f95cb75b58ad885b54e6fd
mdeyanov83 <mdeyanov@gmail.com>
2025-09-24 22:36:32 +0000

    Wed, Sep 24, 2025, 10:36 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
-import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,7 +32,7 @@ class HomePage extends StatelessWidget {
       // future: Firebase.initializeApp(
       //   options: DefaultFirebaseOptions.currentPlatform,
       // ),
-      future
+      future: AuthService.initialize();
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
