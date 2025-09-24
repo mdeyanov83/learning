f8959b91a33726da613e7d4607e59b38ef2eb282
mdeyanov83 <mdeyanov@gmail.com>
2025-09-24 14:07:11 +0000

    Wed, Sep 24, 2025, 2:07 PM +00:00

--- flutter/mynotes/lib/services/auth/auth_user.dart
+++ flutter/mynotes/lib/services/auth/auth_user.dart
@@ -6,4 +6,6 @@ import 'package:flutter/foundation.dart';
 class AuthUser {
   final bool isEmailVerified;
   const AuthUser(this.isEmailVerified);
+
+  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
 }
