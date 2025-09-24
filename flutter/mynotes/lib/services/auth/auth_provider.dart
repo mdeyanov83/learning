0965ff1f96654db6c18c3ad84646614e1aa64d9f
mdeyanov83 <mdeyanov@gmail.com>
2025-09-24 15:08:34 +0000

    Wed, Sep 24, 2025, 3:08 PM +00:00

--- flutter/mynotes/lib/services/auth/auth_provider.dart
+++ flutter/mynotes/lib/services/auth/auth_provider.dart
@@ -1,4 +1,12 @@
 import 'package:mynotes/services/auth/auth_user.dart';
 
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email, 
+    required String password,
 
+
+  })
+}
 
