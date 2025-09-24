fea519f0c7a2105b8ea04571121e22428d55b068
mdeyanov83 <mdeyanov@gmail.com>
2025-09-24 12:12:25 +0000

    Wed, Sep 24, 2025, 12:12 PM +00:00

--- flutter/mynotes/lib/views/verify_email_view.dart
+++ flutter/mynotes/lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
