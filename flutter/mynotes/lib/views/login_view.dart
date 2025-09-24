5415a28de7e6a59f620977e7566ed9572833f274
mdeyanov83 <mdeyanov@gmail.com>
2025-09-24 12:24:42 +0000

    Wed, Sep 24, 2025, 12:24 PM +00:00

--- flutter/mynotes/lib/views/login_view.dart
+++ flutter/mynotes/lib/views/login_view.dart
@@ -66,18 +66,17 @@ class _LoginViewState extends State<LoginView> {
                   password: password,
                 );
                 final user = FirebaseAuth.instance.currentUser;
-                if (user?.emailVerfified ?? false) {
+                if (user?.emailVerified ?? false) {
                   // user's email is verified
                   Navigator.of(
                     context,
                   ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
-                  {
+                } else {
                   // user's email is not verified
                   Navigator.of(
                     context,
                   ).pushNamedAndRemoveUntil(verifyEmailRoute, (route) => false);
                 }
-
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
                   await showErrorDialog(context, 'User not found');
