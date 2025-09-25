5715d46e58d9884a9f1a851cebac592af2516b5f
mdeyanov83 <mdeyanov@gmail.com>
2025-09-25 21:01:51 +0000

    Thu, Sep 25, 2025, 9:01 PM +00:00

--- flutter/mynotes/lib/services/crud/notes_service.dart
+++ flutter/mynotes/lib/services/crud/notes_service.dart
@@ -5,12 +5,22 @@ import 'package:path/path.dart' show join;
 
 class DatabaseAlreadyOpenException implements Exception {}
 class UnableToGetDocumentsDirectory implements Exception {}
+class DatabaseIsNotOpen implements Exception {}
 
 class NotesService {
 
   Database? _db;
 
-  
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      await db.close();
+    }
+
+
+  }
 
   Future<void> open() async {
     if (_db != null) {
