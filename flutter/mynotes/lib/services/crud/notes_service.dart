782679a2b9b3f16d94d3ba2e1b070dd6a38e9719
mdeyanov83 <mdeyanov@gmail.com>
2025-09-25 19:42:27 +0000

    Thu, Sep 25, 2025, 7:42 PM +00:00

--- flutter/mynotes/lib/services/crud/notes_service.dart
+++ flutter/mynotes/lib/services/crud/notes_service.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart'
     show getApplicationDocumentsDirectory;
 import 'package:path/path.dart' show join;
 
+@immutable
 class DatabaseUser {
   final int id;
   final String email;
