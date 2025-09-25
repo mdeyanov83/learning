b34e098c0069efb44bcd867e04c88223462780d5
mdeyanov83 <mdeyanov@gmail.com>
2025-09-25 19:17:37 +0000

    Thu, Sep 25, 2025, 7:17 PM +00:00

--- flutter/mynotes/lib/services/crud/notes_service.dart
+++ flutter/mynotes/lib/services/crud/notes_service.dart
@@ -1,5 +1,5 @@
 import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart';
-
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
+import 'package:path/path.dart' show join;
 
