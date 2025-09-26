3c815ce8b8f934b770aa30fb32e78b3037e8cbde
mdeyanov83 <mdeyanov@gmail.com>
2025-09-26 13:20:04 +0000

    Fri, Sep 26, 2025, 1:20 PM +00:00

--- flutter/mynotes/lib/views/notes_view.dart
+++ flutter/mynotes/lib/views/notes_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/enums/menu_action.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/crud/notes_service.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -12,13 +13,21 @@ class NotesView extends StatefulWidget {
 
 class _NotesViewState extends State<NotesView> {
 
-  late final NotesService = _notesService;
+  late final NotesService _notesService;
   String get userEmail => AuthService.firebase().currentUser!.email!;
+
 
   @override
   void initState() {
-
+    _notesService = NotesService();
+    _notesService.open();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _notesService.close();
+    super.dispose();
   }
 
   @override
