a8a706a44c64ebadfb590e925bbb1829feed94d6
mdeyanov83 <mdeyanov@gmail.com>
2025-09-26 13:15:13 +0000

    Fri, Sep 26, 2025, 1:15 PM +00:00

--- flutter/mynotes/lib/views/notes_view.dart
+++ flutter/mynotes/lib/views/notes_view.dart
@@ -12,8 +12,7 @@ class NotesView extends StatefulWidget {
 
 class _NotesViewState extends State<NotesView> {
 
-  String get userEmail => AuthService.firebase().currentUser!;
-
+  String get userEmail => AuthService.firebase().currentUser!.email!;
 
   @override
   Widget build(BuildContext context) {
