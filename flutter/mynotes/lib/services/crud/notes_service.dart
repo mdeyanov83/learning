c7b945591e4e9a0969cdfe785799579c43f1e32b
mdeyanov83 <mdeyanov@gmail.com>
2025-09-26 11:15:36 +0000

    Fri, Sep 26, 2025, 11:15 AM +00:00

--- flutter/mynotes/lib/services/crud/notes_service.dart
+++ flutter/mynotes/lib/services/crud/notes_service.dart
@@ -189,6 +189,7 @@ class NotesService {
       await db.execute(createUserTable);
       // create the note table
       await db.execute(createNoteTable);
+      await _cacheNotes();
     } on MissingPlatformDirectoryException {
       throw UnableToGetDocumentsDirectory();
     }
