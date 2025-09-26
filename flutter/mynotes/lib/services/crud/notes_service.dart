685afb3f88ba5ebc90dd44b29b8cb4638ad7c40a
mdeyanov83 <mdeyanov@gmail.com>
2025-09-26 13:11:24 +0000

    Fri, Sep 26, 2025, 1:11 PM +00:00

--- flutter/mynotes/lib/services/crud/notes_service.dart
+++ flutter/mynotes/lib/services/crud/notes_service.dart
@@ -103,7 +103,6 @@ class NotesService {
     if (deletedCount == 0) {
       throw CouldNotDeleteNote();
     } else {
-      final countBefore = _notes.length;
       _notes.removeWhere((note) => note.id == id);
       _notesStreamController.add(_notes);
     }
