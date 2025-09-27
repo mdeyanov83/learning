3f35c40b00247122502fb9717f6bf3c0e2b379be
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 21:14:13 +0000

    Sat, Sep 27, 2025, 9:14 PM +00:00

--- flutter/mynotes/lib/utilities/dialogs/error_dialog.dart
+++ flutter/mynotes/lib/utilities/dialogs/error_dialog.dart
@@ -1,5 +1,10 @@
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
 
-Future<void> showErrorDialog(
-  BuildContxt context,
-  String text
-)
+Future<void> showErrorDialog(BuildContxt context, String text) {
+  return showGenericDialog(
+    context: context,
+    title: title,
+    content: content,
+    optionsBuilder: optionsBuilder,
+  );
+}
