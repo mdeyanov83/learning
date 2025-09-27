ccb32805d11ffeea0460dd9f8018fc7873194529
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 19:55:08 +0000

    Sat, Sep 27, 2025, 7:55 PM +00:00

---
+++ flutter/mynotes/lib/views/notes/notes_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
