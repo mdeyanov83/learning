6726cbe4f061eac99cc2a9b80e1ca02763a97b64
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 22:35:34 +0000

    Sat, Sep 27, 2025, 10:35 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -19,7 +19,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => VerifyEmailView(),
-        newNoteRoute: (context) => const CreateUpdateNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
