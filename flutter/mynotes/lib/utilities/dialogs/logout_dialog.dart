5ca311d96aded7a2f7b9a5e0ed1d8f89b589b641
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 21:26:28 +0000

    Sat, Sep 27, 2025, 9:26 PM +00:00

---
+++ flutter/mynotes/lib/utilities/dialogs/logout_dialog.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showLogoutDialog(BuildContext context) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: title,
+    content: content,
+    optionsBuilder: optionsBuilder,
+  );
+}
