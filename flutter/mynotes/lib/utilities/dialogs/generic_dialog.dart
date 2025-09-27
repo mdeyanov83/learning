ea27605a06b33ac2ff439d04fb8c6841897b9160
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 21:10:00 +0000

    Sat, Sep 27, 2025, 9:10 PM +00:00

--- flutter/mynotes/lib/utilities/dialogs/generic_dialog.dart
+++ flutter/mynotes/lib/utilities/dialogs/generic_dialog.dart
@@ -10,7 +10,6 @@ Future<T?> showGenericDialog<T>({
 }) {
   final options = optionsBuilder();
 
-
   return showDialog<T>(
     context: context,
     builder: (context) {
@@ -18,18 +17,19 @@ Future<T?> showGenericDialog<T>({
         title: Text(title),
         content: Text(content),
         actions: options.keys.map((optionTitle) {
-          final T value = options[optionTitle];
+          final value = options[optionTitle];
           return TextButton(
             onPressed: () {
-
+              if (value != null) {
+                Navigator.of(context).pop(value);
+              } else {
+                Navigator.of(context).pop();
+              }
             },
-            
+            child: Text(optionTitle),
           );
-
         }).toList(),
       );
-
     },
   );
-
 }
