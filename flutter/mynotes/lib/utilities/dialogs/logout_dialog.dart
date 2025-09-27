4f6c3b6d350999a8a057ec7cde510477b7321412
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 21:28:46 +0000

    Sat, Sep 27, 2025, 9:28 PM +00:00

--- flutter/mynotes/lib/utilities/dialogs/logout_dialog.dart
+++ flutter/mynotes/lib/utilities/dialogs/logout_dialog.dart
@@ -4,8 +4,11 @@ import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
 Future<bool> showLogoutDialog(BuildContext context) {
   return showGenericDialog<bool>(
     context: context,
-    title: title,
-    content: content,
-    optionsBuilder: optionsBuilder,
-  );
+    title: 'Log out',
+    content: 'Are you sure you want to log out?',
+    optionsBuilder: () => {
+      'Cancel': false,
+      'Log out': true,
+    },
+  ).then((value) => value ?? false);
 }
