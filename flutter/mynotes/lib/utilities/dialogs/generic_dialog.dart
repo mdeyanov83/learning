27f1126a5996f98dc598e4f7f765c2bc9193624f
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 20:47:32 +0000

    Sat, Sep 27, 2025, 8:47 PM +00:00

---
+++ flutter/mynotes/lib/utilities/dialogs/generic_dialog.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Future<T?> showGenericDialog<T>({
+  required BuildContext context,
+  required String title,
+  required String content,
+  required 
+}) {}
