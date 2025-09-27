f5443975bc4d5a963a9cdb9d128ae02b92d3ac89
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 20:13:50 +0000

    Sat, Sep 27, 2025, 8:13 PM +00:00

--- flutter/mynotes/lib/views/notes/notes_list_view.dart
+++ flutter/mynotes/lib/views/notes/notes_list_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/services/crud/notes_service.dart';
 
 class NotesListView extends StatelessWidget {
-
   final List<DatabaseNote> notes;
 
   const NotesListView({ Key? key }) : super(key: key);
