972ce9e41180875cbe35b4063b6a9d3ecbcae2b6
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 21:22:52 +0000

    Sat, Sep 27, 2025, 9:22 PM +00:00

--- flutter/mynotes/lib/views/login_view.dart
+++ flutter/mynotes/lib/views/login_view.dart
@@ -3,7 +3,6 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
-import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
