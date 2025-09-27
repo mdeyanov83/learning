8e26e2e736a47482dcbda8cd9e1fee089cacbcb8
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 20:52:57 +0000

    Sat, Sep 27, 2025, 8:52 PM +00:00

--- flutter/mynotes/lib/utilities/dialogs/generic_dialog.dart
+++ flutter/mynotes/lib/utilities/dialogs/generic_dialog.dart
@@ -6,5 +6,21 @@ Future<T?> showGenericDialog<T>({
   required BuildContext context,
   required String title,
   required String content,
-  required
-}) {}
+  required DialogOptionBuilder optionsBuilder,
+}) {
+  final options = optionsBuilder();
+
+
+  return showDialog<T>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: options
+      ),
+
+    },
+  );
+
+}
