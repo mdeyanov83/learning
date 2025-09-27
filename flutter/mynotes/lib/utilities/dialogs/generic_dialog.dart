b767e2bd055bbf7429a974751829e12aecd79ced
mdeyanov83 <mdeyanov@gmail.com>
2025-09-27 20:49:36 +0000

    Sat, Sep 27, 2025, 8:49 PM +00:00

--- flutter/mynotes/lib/utilities/dialogs/generic_dialog.dart
+++ flutter/mynotes/lib/utilities/dialogs/generic_dialog.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+typedef DialogOptionBuilder<T> = Map<String, T?> Function();
+
 Future<T?> showGenericDialog<T>({
   required BuildContext context,
   required String title,
   required String content,
-  required 
+  required
 }) {}
