2cb40e53b60a5eaeea57eeee174acaf103008382
mdeyanov83 <mdeyanov@gmail.com>
2025-09-28 21:55:39 +0000

    Sun, Sep 28, 2025, 9:55 PM +00:00

--- flutter/mynotes/lib/extensions/list/filter.dart
+++ flutter/mynotes/lib/extensions/list/filter.dart
@@ -1,4 +1,4 @@
 extension Filter<T> on Stream {
-  Stream<List<T>> filer(bool Function(T) where) =>
+  Stream<List<T>> filter(bool Function(T) where) =>
     map((items) => items.where(where).toList());
 }
