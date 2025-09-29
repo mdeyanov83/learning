bd6576ede95942fb49d075d1d5f3272435f35eab
mdeyanov83 <mdeyanov@gmail.com>
2025-09-29 22:51:01 +0000

    Mon, Sep 29, 2025, 10:51 PM +00:00

--- flutter/mynotes/lib/views/notes/notes_view.dart
+++ flutter/mynotes/lib/views/notes/notes_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/enums/menu_action.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:mynotes/utilities/dialogs/logout_dialog.dart';
 import 'package:mynotes/views/notes/notes_list_view.dart';
@@ -62,17 +63,17 @@ class _NotesViewState extends State<NotesView> {
         ],
       ),
       body: StreamBuilder(
-        stream: _notesService.allNotes,
+        stream: _notesService.allNotes(ownerUserId: userId),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
             case ConnectionState.active:
               if (snapshot.hasData) {
-                final allNotes = snapshot.data as List<DatabaseNote>;
+                final allNotes = snapshot.data as Iterable<CloudNote>;
                 return NotesListView(
                   notes: allNotes,
                   onDeleteNote: (note) async {
-                    await _notesService.deleteNote(id: note.id);
+                    await _notesService.deleteNote(documentId: note.documentId);
                   },
                   onTap: (note) {
                     Navigator.of(context).pushNamed(
