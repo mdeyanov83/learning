258f460c8c0fd86d7c60f11ac30720a1ce4d3c0b
mdeyanov83 <mdeyanov@gmail.com>
2025-09-29 21:11:21 +0000

    Mon, Sep 29, 2025, 9:11 PM +00:00

--- flutter/mynotes/lib/services/cloud/firebase_cloud_storage.dart
+++ flutter/mynotes/lib/services/cloud/firebase_cloud_storage.dart
@@ -1,14 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/cloud_storage_constants.dart';
+import 'package:mynotes/services/cloud/cloud_storage_exceptions.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
 
   Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
-    notes.where
-
-
+    try {
+      await notes.where(ownerUserIdFieldName, isEqualTo: ownerUserId).get().then((value) => null);
+    } catch (e) {
+      throw CouldNotGetAllNotesException();
+    }
   }
 
   void createNewNote({required String ownerUserId}) async {
@@ -18,7 +21,8 @@ class FirebaseCloudStorage {
     });
   }
 
-  static final FirebaseCloudStorage _shared = FirebaseCloudStorage._sharedInstance();
+  static final FirebaseCloudStorage _shared =
+      FirebaseCloudStorage._sharedInstance();
   FirebaseCloudStorage._sharedInstance();
   factory FirebaseCloudStorage() => _shared;
 }
