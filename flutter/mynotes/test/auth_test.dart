54d91542f4af3c20cfb94f30fae2608e03fb1311
mdeyanov83 <mdeyanov@gmail.com>
2025-09-29 14:46:52 +0000

    Mon, Sep 29, 2025, 2:46 PM +00:00

--- flutter/mynotes/test/auth_test.dart
+++ flutter/mynotes/test/auth_test.dart
@@ -120,6 +120,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
+      id: 'my_id',
       email: 'foo@bar.com',
       isEmailVerified: false,
     );
@@ -140,6 +141,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
+      id: 'my_id',
       email: 'foo@bar.com',
       isEmailVerified: true,
     );
