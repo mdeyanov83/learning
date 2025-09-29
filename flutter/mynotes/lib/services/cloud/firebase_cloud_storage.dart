edd2a1b86090d11028d56c5fea622d7970a63549
mdeyanov83 <mdeyanov@gmail.com>
2025-09-29 20:58:24 +0000

    Mon, Sep 29, 2025, 8:58 PM +00:00

--- flutter/mynotes/lib/services/cloud/firebase_cloud_storage.dart
+++ flutter/mynotes/lib/services/cloud/firebase_cloud_storage.dart
@@ -1,9 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mynotes/services/cloud/cloud_storage_constants.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
 
-  void
+  void createNewNote({required String ownerUserId}) async {
+    await notes.add({
+      ownerUserIdFieldName: ownerUserId,
+      textFieldName: '',
+    });
+  }
 
   static final FirebaseCloudStorage _shared = FirebaseCloudStorage._sharedInstance();
   FirebaseCloudStorage._sharedInstance();
