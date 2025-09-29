1e8fb596a35ad1b6631ddddf38910e32b35089b9
mdeyanov83 <mdeyanov@gmail.com>
2025-09-29 21:00:30 +0000

    Mon, Sep 29, 2025, 9:00 PM +00:00

--- flutter/mynotes/lib/services/cloud/firebase_cloud_storage.dart
+++ flutter/mynotes/lib/services/cloud/firebase_cloud_storage.dart
@@ -1,8 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/cloud_storage_constants.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    notes.where
+
+
+  }
 
   void createNewNote({required String ownerUserId}) async {
     await notes.add({
