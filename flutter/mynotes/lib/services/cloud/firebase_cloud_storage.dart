95f50837cc85a89aedb4e52c7a9bc5957dbc4813
mdeyanov83 <mdeyanov@gmail.com>
2025-09-29 20:55:39 +0000

    Mon, Sep 29, 2025, 8:55 PM +00:00

--- flutter/mynotes/lib/services/cloud/firebase_cloud_storage.dart
+++ flutter/mynotes/lib/services/cloud/firebase_cloud_storage.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class FirebaseCloudStorage {
+  final notes = FirebaseFirestore.instance.collection('notes');
+
+  void
 
   static final FirebaseCloudStorage _shared = FirebaseCloudStorage._sharedInstance();
   FirebaseCloudStorage._sharedInstance();
