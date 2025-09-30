345b5e3db6a9057d0d8b9eab174960c24bb51d4e
mdeyanov83 <mdeyanov@gmail.com>
2025-09-30 15:31:28 +0000

    Tue, Sep 30, 2025, 3:31 PM +00:00

--- flutter/mynotes/lib/main.dart
+++ flutter/mynotes/lib/main.dart
@@ -67,3 +67,21 @@ class _HomePageState extends State<HomePage> {
     return const Placeholder();
   }
 }
+
+@immutable
+abstract class CounterState {
+  final int value;
+  const CounterState(this.value);
+}
+
+class CounterStateValid extends CounterState {
+  const CounterStateValid(super.value);
+}
+
+class CounterStateInvalidNumber extends CounterState {
+  final String invalidValue;
+  const CounterStateInvalidNumber({
+    required this.invalidValue,
+    required int previousValue,
+  });
+}
