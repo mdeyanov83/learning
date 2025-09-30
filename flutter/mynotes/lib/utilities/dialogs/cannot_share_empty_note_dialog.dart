e581ec31482c507582400e708a143894422cf2e6
mdeyanov83 <mdeyanov@gmail.com>
2025-09-30 11:56:40 +0000

    Tue, Sep 30, 2025, 11:56 AM +00:00

--- flutter/mynotes/lib/utilities/dialogs/cannot_share_empty_note_dialog.dart
+++ flutter/mynotes/lib/utilities/dialogs/cannot_share_empty_note_dialog.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialogs/generic_dialog.dart';
+
+
 
 Future<void> showCannotShareEmtpyNoteDialog(BuildContext context) {
-
+  return showGenericDialog(
+    context: context,
+    title: 'Sharing',
+    content: 'You cannot share an empty note!',
+    optionsBuilder: () => {
+      'OK': null,
+    },
+  );
 }
