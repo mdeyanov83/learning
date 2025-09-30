59f0b1cb3b086a47ea6a088bc0875f37317bb481
mdeyanov83 <mdeyanov@gmail.com>
2025-09-30 23:52:36 +0000

    Tue, Sep 30, 2025, 11:52 PM +00:00

---
+++ flutter/mynotes/lib/services/auth/bloc/auth_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+    on<AuthEventInitialize>((event, emit) async {
+      await provider.initialize();
+      final user = provider.currentUser;
+
+    });
+
+  }
+}
