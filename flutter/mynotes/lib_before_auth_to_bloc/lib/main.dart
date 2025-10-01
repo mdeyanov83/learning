8932617ac6b7ed742006587178f76c609dce2986
mdeyanov83 <mdeyanov@gmail.com>
2025-10-01 01:03:28 +0000

    Wed, Oct 1, 2025, 1:03 AM +00:00

--- flutter/mynotes/lib_before_auth_to_bloc/lib/main.dart
+++ flutter/mynotes/lib_before_auth_to_bloc/lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
-import 'package:mynotes/services/auth/bloc/auth_event.dart';
-import 'package:mynotes/services/auth/bloc/auth_state.dart';
-import 'package:mynotes/services/auth/firebase_auth_provider.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes/create_update_note_view.dart';
 import 'package:mynotes/views/notes/notes_view.dart';
