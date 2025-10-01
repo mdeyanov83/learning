a4f48373004d4f579de8ae5d7703ad0a51ee9305
mdeyanov83 <mdeyanov@gmail.com>
2025-10-01 01:08:13 +0000

    Wed, Oct 1, 2025, 1:08 AM +00:00

--- flutter/mynotes/lib/views/login_view.dart
+++ flutter/mynotes/lib/views/login_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -60,22 +62,28 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                await AuthService.firebase().logIn(
-                  email: email,
-                  password: password,
+                context.read<AuthBlock>().add(
+                  AuthEventLogin(email, password,),
                 );
-                final user = AuthService.firebase().currentUser;
-                if (user?.isEmailVerified ?? false) {
-                  // user's email is verified
-                  Navigator.of(
-                    context,
-                  ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
-                } else {
-                  // user's email is not verified
-                  Navigator.of(
-                    context,
-                  ).pushNamedAndRemoveUntil(verifyEmailRoute, (route) => false);
-                }
+
+
+
+                // await AuthService.firebase().logIn(
+                //   email: email,
+                //   password: password,
+                // );
+                // final user = AuthService.firebase().currentUser;
+                // if (user?.isEmailVerified ?? false) {
+                //   // user's email is verified
+                //   Navigator.of(
+                //     context,
+                //   ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
+                // } else {
+                //   // user's email is not verified
+                //   Navigator.of(
+                //     context,
+                //   ).pushNamedAndRemoveUntil(verifyEmailRoute, (route) => false);
+                // }
               } on UserNotFoundAuthException {
                 await showErrorDialog(
                   context,
