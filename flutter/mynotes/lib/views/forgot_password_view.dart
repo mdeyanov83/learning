a757e93b8c21730eb10e0466f29490d425192c48
mdeyanov83 <mdeyanov@gmail.com>
2025-10-02 19:11:22 +0000

    Thu, Oct 2, 2025, 7:11 PM +00:00

--- flutter/mynotes/lib/views/forgot_password_view.dart
+++ flutter/mynotes/lib/views/forgot_password_view.dart
@@ -8,6 +8,20 @@ class ForgotPasswordView extends StatefulWidget {
 }
 
 class _ForgotPasswordViewState extends State<ForgotPasswordView> {
+
+  late final TextEditingController _controller;
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
