c749de880921f5583adcdb3670d2f53b653354c5
mdeyanov83 <mdeyanov@gmail.com>
2025-10-02 11:52:40 +0000

    Thu, Oct 2, 2025, 11:52 AM +00:00

--- flutter/mynotes/lib/helpers/loading/loading_screen.dart
+++ flutter/mynotes/lib/helpers/loading/loading_screen.dart
@@ -1,7 +1,18 @@
-class LoadingScreen {
+import 'package:mynotes/helpers/loading/loading_screen_controller.dart';
 
+class LoadingScreen {
+  factory LoadingScreen() => _shared;
   static final LoadingScreen _shared = LoadingScreen._sharedInstance();
   LoadingScreen._sharedInstance();
-  factory LoadingScreen() => _shared;
+
+  LoadingScreenController? controller;
+
+  LoadingScreenController showOverlay({
+    required BuildContext: context,
+    required text: text,
+  }
+
+
+  )
 
 }
