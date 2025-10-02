8922a7c87e005b43ccd1fe1b30c8f20f5276b921
mdeyanov83 <mdeyanov@gmail.com>
2025-10-02 16:39:05 +0000

    Thu, Oct 2, 2025, 4:39 PM +00:00

--- flutter/mynotes/lib/services/auth/bloc/auth_state.dart
+++ flutter/mynotes/lib/services/auth/bloc/auth_state.dart
@@ -41,7 +41,7 @@ class AuthStateLoggedOut extends AuthState with EquatableMixin {
   const AuthStateLoggedOut({
     required this.exception,
     required super.isLoading,
-    String? loadingText
+    super.loadingText
   });
 
   @override
