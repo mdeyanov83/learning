3fdbf808ea888a6943505e150f970713267483c3
mdeyanov83 <mdeyanov@gmail.com>
2025-10-02 19:12:02 +0000

    Thu, Oct 2, 2025, 7:12 PM +00:00

--- flutter/mynotes/lib/views/forgot_password_view.dart
+++ flutter/mynotes/lib/views/forgot_password_view.dart
@@ -24,6 +24,6 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
