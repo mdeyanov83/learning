13949e13223ec694ab68a58ffaf37ec90ff6402a
mdeyanov83 <mdeyanov@gmail.com>
2025-10-02 12:36:13 +0000

    Thu, Oct 2, 2025, 12:36 PM +00:00

--- flutter/mynotes/lib/helpers/loading/loading_screen.dart
+++ flutter/mynotes/lib/helpers/loading/loading_screen.dart
@@ -13,7 +13,6 @@ class LoadingScreen {
     required BuildContext context,
     required String text,
   }) {
-
     final _text = StreamController<String>();
     _text.add(text);
 
@@ -21,11 +20,31 @@ class LoadingScreen {
     final renderBox = context.findRenderObject() as RenderBox;
     final size = renderBox.size;
 
-    final overlay = OverlayEntry(builder: (context) {
-      return Material
-
-
-    });
+    final overlay = OverlayEntry(
+      builder: (context) {
+        return Material(
+          color: Colors.black.withAlpha(150),
+          child: Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: size.width * 0.8,
+                maxHeight: size.height * 0.8,
+                minWidth: size.width * 0.5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  
+                ),
+              )
+            ),
+          ),
+        );
+      },
+    );
   }
-
 }
