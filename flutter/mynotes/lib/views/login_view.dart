cb7661e6d760cacc6f0cd64419c53c8d4ab1aeab
mdeyanov83 <mdeyanov@gmail.com>
2025-10-02 20:22:17 +0000

    Thu, Oct 2, 2025, 8:22 PM +00:00

--- flutter/mynotes/lib/views/login_view.dart
+++ flutter/mynotes/lib/views/login_view.dart
@@ -36,7 +36,7 @@ class _LoginViewState extends State<LoginView> {
       listener: (context, state) async {
         if (state is AuthStateLoggedOut) {
           if (state.exception is UserNotFoundAuthException) {
-            await showErrorDialog(context, 'User not found');
+            await showErrorDialog(context, 'Cannot find a user with the entered credentials');
           } else if (state.exception is WrongPasswordAuthException) {
             await showErrorDialog(context, 'Wrong credentials');
           } else if (state.exception is GenericAuthException) {
