90437aefcfc8bb7fb3c1f705e08edfd49f1db925
mdeyanov83 <mdeyanov@gmail.com>
2025-10-05 21:01:39 +0000

    Sun, Oct 5, 2025, 9:01 PM +00:00

---
+++ flutter/mynotes/lib_before_localizations/services/auth/bloc/auth_state.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:mynotes/services/auth/auth_user.dart';
+import 'package:equatable/equatable.dart';
+
+@immutable
+abstract class AuthState {
+  final bool isLoading;
+  final String? loadingText;
+  const AuthState({
+    required this.isLoading,
+    this.loadingText = 'Please wait a moment',
+  });
+}
+
+class AuthStateUninitialized extends AuthState {
+  const AuthStateUninitialized({required super.isLoading});
+}
+
+class AuthStateRegistering extends AuthState {
+  final Exception? exception;
+  const AuthStateRegistering({
+    required this.exception,
+    required super.isLoading,
+  });
+}
+
+class AuthStateForgotPassword extends AuthState {
+  final Exception? exception;
+  final bool hasSentEmail;
+  const AuthStateForgotPassword({
+    required super.isLoading,
+    required this.exception,
+    required this.hasSentEmail,
+  });
+}
+
+class AuthStateLoggedIn extends AuthState {
+  final AuthUser user;
+  const AuthStateLoggedIn({
+    required this.user,
+    required super.isLoading,
+  });
+}
+
+class AuthStateNeedsVerification extends AuthState {
+  const AuthStateNeedsVerification({required super.isLoading});
+}
+
+class AuthStateLoggedOut extends AuthState with EquatableMixin {
+  final Exception? exception;
+  const AuthStateLoggedOut({
+    required this.exception,
+    required super.isLoading,
+    super.loadingText,
+  });
+
+  @override
+  List<Object?> get props => [exception, isLoading];
+}
