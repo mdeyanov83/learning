e39edf87c63116b8e1b604f4371622b7e3532840
mdeyanov83 <mdeyanov@gmail.com>
2025-10-05 21:26:32 +0000

    Sun, Oct 5, 2025, 9:26 PM +00:00

--- flutter/mynotes/lib/views/verify_email_view.dart
+++ flutter/mynotes/lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mynotes/extensions/buildcontext/loc.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
 
@@ -14,7 +15,7 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Verify email')),
+      appBar: AppBar(title: Text(context.loc.verify_email)),
       body: SingleChildScrollView(
         child: Column(
           children: [
