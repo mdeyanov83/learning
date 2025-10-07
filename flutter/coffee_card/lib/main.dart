6eb2536eb5818c09ce9e1224a7249785db5898cd
mdeyanov83 <mdeyanov@gmail.com>
2025-10-07 13:41:15 +0000

    Tue, Oct 7, 2025, 1:41 PM +00:00

--- flutter/coffee_card/lib/main.dart
+++ flutter/coffee_card/lib/main.dart
@@ -20,6 +20,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello, Ninjas');
+    return const Text('Hello, Ninjas...');
   }
 }
