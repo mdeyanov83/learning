88db7bec68d995776cf04ae01608d6c4b9a2a4e2
mdeyanov83 <mdeyanov@gmail.com>
2025-10-07 21:00:37 +0000

    Tue, Oct 7, 2025, 9:00 PM +00:00

--- flutter/coffee_card/lib/main.dart
+++ flutter/coffee_card/lib/main.dart
@@ -39,6 +39,7 @@ class Sandbox extends StatelessWidget {
             color: Colors.blue,
             child: const Text('three'),
           ),
+          Image.asset('assets/img/coffee_bg.jpg'),
         ],
       ),
     );
