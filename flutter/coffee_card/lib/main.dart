951716c9dd61325130e87548b07f94168212dcd3
mdeyanov83 <mdeyanov@gmail.com>
2025-10-07 12:42:21 +0000

    Tue, Oct 7, 2025, 12:42 PM +00:00

--- flutter/coffee_card/lib/main.dart
+++ flutter/coffee_card/lib/main.dart
@@ -14,3 +14,13 @@ void main() {
     ),
   );
 }
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
