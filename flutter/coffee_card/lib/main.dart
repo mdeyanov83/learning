c038064a800c594eb1af2535e84d9f295884d11b
mdeyanov83 <mdeyanov@gmail.com>
2025-10-07 19:29:07 +0000

    Tue, Oct 7, 2025, 7:29 PM +00:00

--- flutter/coffee_card/lib/main.dart
+++ flutter/coffee_card/lib/main.dart
@@ -9,3 +9,17 @@ void main() {
   );
 }
 
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: const Text('Hello'),
+    );
+  }
+}
