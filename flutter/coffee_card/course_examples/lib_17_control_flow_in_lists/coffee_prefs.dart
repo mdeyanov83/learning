3792b8c416631772d33789eac499a0c661c6e56a
mdeyanov83 <mdeyanov@gmail.com>
2025-10-08 10:55:30 +0000

    Wed, Oct 8, 2025, 10:55 AM +00:00

---
+++ flutter/coffee_card/course_examples/lib_17_control_flow_in_lists/coffee_prefs.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class CoffeePrefs extends StatefulWidget {
+  const CoffeePrefs({super.key});
+
+  @override
+  State<CoffeePrefs> createState() => _CoffeePrefsState();
+}
+
+class _CoffeePrefsState extends State<CoffeePrefs> {
+  int strength = 1;
+  int sugars = 1;
+
+  void increaseStrength() {
+    setState(() {
+      strength = strength < 5 ? strength + 1 : 1;
+    });
+  }
+
+  void increaseSugars() {
+    setState(() {
+      sugars = sugars < 5 ? sugars + 1 : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            const Text('Strength: '),
+            for (int i = 0; i < strength; i++)
+              Image.asset(
+                'assets/img/coffee_bean.png',
+                width: 25,
+                color: Colors.brown[100],
+                colorBlendMode: BlendMode.multiply,
+              ),
+
+            Expanded(child: SizedBox()),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseStrength,
+              child: const Text('+'),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            const Text('Sugars: '),
+            if (sugars == 0)
+              const Text('No sugars'),
+            for (int i = 0; i < sugars; i++)
+              Image.asset(
+                'assets/img/sugar_cube.png',
+                width: 25,
+                color: Colors.brown[100],
+                colorBlendMode: BlendMode.multiply,
+              ),
+            const Expanded(
+              child: SizedBox(),
+            ),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseSugars,
+              child: const Text('+'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
