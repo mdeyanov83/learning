54530f86a89eed16673624bc8069b03611573921
mdeyanov83 <mdeyanov@gmail.com>
2025-10-08 11:33:53 +0000

    Wed, Oct 8, 2025, 11:33 AM +00:00

--- flutter/coffee_card/lib/styled_button.dart
+++ flutter/coffee_card/lib/styled_button.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 class StyledButton extends StatelessWidget {
-  const StyledButton(
-    {super.key});
+  const StyledButton({
+    super.key,
+    required this.onPressed,
+    required this.child,
+  });
 
-  final String text;
-
-
+  final Widget child;
+  final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextButton(
+      onPressed: onPressed,
+      child: child,
+      style: TextButton.styleFrom(
+         backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+      ),
+    );
   }
 }
