ba1939748eb0fd5d4b96ad176b209ac9b2fdc5a5
mdeyanov83 <mdeyanov@gmail.com>
2025-10-08 10:54:30 +0000

    Wed, Oct 8, 2025, 10:54 AM +00:00

--- flutter/coffee_card/lib/coffee_prefs.dart
+++ flutter/coffee_card/lib/coffee_prefs.dart
@@ -8,7 +8,6 @@ class CoffeePrefs extends StatefulWidget {
 }
 
 class _CoffeePrefsState extends State<CoffeePrefs> {
-
   int strength = 1;
   int sugars = 1;
 
@@ -31,13 +30,14 @@ class _CoffeePrefsState extends State<CoffeePrefs> {
         Row(
           children: [
             const Text('Strength: '),
-            Text('$strength'),
-            Image.asset(
-              'assets/img/coffee_bean.png',
-              width: 25,
-              color: Colors.brown[100],
-              colorBlendMode: BlendMode.multiply,
-            ),
+            for (int i = 0; i < strength; i++)
+              Image.asset(
+                'assets/img/coffee_bean.png',
+                width: 25,
+                color: Colors.brown[100],
+                colorBlendMode: BlendMode.multiply,
+              ),
+
             Expanded(child: SizedBox()),
             FilledButton(
               style: FilledButton.styleFrom(
@@ -52,13 +52,15 @@ class _CoffeePrefsState extends State<CoffeePrefs> {
         Row(
           children: [
             const Text('Sugars: '),
-            Text('$sugars'),
-            Image.asset(
-              'assets/img/sugar_cube.png',
-              width: 25,
-              color: Colors.brown[100],
-              colorBlendMode: BlendMode.multiply,
-            ),
+            if (sugars == 0)
+              const Text('No sugars'),
+            for (int i = 0; i < sugars; i++)
+              Image.asset(
+                'assets/img/sugar_cube.png',
+                width: 25,
+                color: Colors.brown[100],
+                colorBlendMode: BlendMode.multiply,
+              ),
             const Expanded(
               child: SizedBox(),
             ),
