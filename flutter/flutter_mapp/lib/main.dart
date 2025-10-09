307e2dc0e0222fc72956c6e896bfa12913c9cf6d
mdeyanov83 <mdeyanov@gmail.com>
2025-10-09 23:04:24 +0000

    Thu, Oct 9, 2025, 11:04 PM +00:00

--- flutter/flutter_mapp/lib/main.dart
+++ flutter/flutter_mapp/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: (value) {},
+          onPressed: () {},
           child: Icon(Icons.add),
         ),
         bottomNavigationBar: NavigationBar(
