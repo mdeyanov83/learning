326e4d21611e79b952c7172f47fd3479164adaf6
mdeyanov83 <mdeyanov@gmail.com>
2025-10-10 10:01:41 +0000

    Fri, Oct 10, 2025, 10:01 AM +00:00

--- flutter/flutter_mapp/lib/main.dart
+++ flutter/flutter_mapp/lib/main.dart
@@ -17,7 +17,23 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text('Flutter Mapp'),
           centerTitle: true,
@@ -36,7 +52,6 @@ class MyApp extends StatelessWidget {
           onDestinationSelected: (int value) {},
           selectedIndex: 0,
         ),
-      ),
-    );
+      );
   }
 }
