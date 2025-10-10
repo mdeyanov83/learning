ec6b1ad850ae0e7bccc3736448b6503032fe831a
mdeyanov83 <mdeyanov@gmail.com>
2025-10-10 12:56:00 +0000

    Fri, Oct 10, 2025, 12:56 PM +00:00

--- flutter/flutter_mapp/lib/data/notifiers.dart
+++ flutter/flutter_mapp/lib/data/notifiers.dart
@@ -1,2 +1,6 @@
 // ValueNotifier: holds the data
 // ValueListenableBuilder: listens to the data (don't need the setState())
+
+import 'package:flutter/material.dart';
+
+ValueNotifier<int> selectedPageNotifier = ValueNotifier(0);
