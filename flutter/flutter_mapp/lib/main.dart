606ac70c0bf050e2ba883db101af954093589bf1
mdeyanov83 <mdeyanov@gmail.com>
2025-10-10 10:03:13 +0000

    Fri, Oct 10, 2025, 10:03 AM +00:00

--- flutter/flutter_mapp/lib/main.dart
+++ flutter/flutter_mapp/lib/main.dart
@@ -31,6 +31,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,8 +52,11 @@ class _MyHomePageState extends State<MyHomePage> {
               label: 'Profile',
             ),
           ],
-          onDestinationSelected: (int value) {},
-          selectedIndex: 0,
+          onDestinationSelected: (int value) {
+            currentIndex = value;
+            setState() )
+          },
+          selectedIndex: currentIndex,
         ),
       );
   }
