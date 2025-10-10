3d90e21805c1f7ab2b654df20ea0da0bb114b0fa
mdeyanov83 <mdeyanov@gmail.com>
2025-10-10 22:39:47 +0000

    Fri, Oct 10, 2025, 10:39 PM +00:00

--- flutter/flutter_mapp/lib/views/pages/profile_page.dart
+++ flutter/flutter_mapp/lib/views/pages/profile_page.dart
@@ -24,6 +24,7 @@ class _ProfilePageState extends State<ProfilePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             DropdownButton(
+              name:
               value: menuItem,
               items: [
                 DropdownMenuItem(
