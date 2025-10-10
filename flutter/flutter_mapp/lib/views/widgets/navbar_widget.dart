696ea4526df3ddee07234e05e30edac02551abda
mdeyanov83 <mdeyanov@gmail.com>
2025-10-10 13:25:41 +0000

    Fri, Oct 10, 2025, 1:25 PM +00:00

--- flutter/flutter_mapp/lib/views/widgets/navbar_widget.dart
+++ flutter/flutter_mapp/lib/views/widgets/navbar_widget.dart
@@ -26,7 +26,7 @@ class _NavBarWidgetState extends State<NavBarWidget> {
             ),
           ],
           onDestinationSelected: (int value) {
-
+            selectedPageNotifier.value = value;
           },
           selectedIndex: selectedPage,
         );
