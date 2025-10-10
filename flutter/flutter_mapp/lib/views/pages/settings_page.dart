d9bb37e8dea2673c5fb7248d61a265935cefda1f
mdeyanov83 <mdeyanov@gmail.com>
2025-10-10 23:08:10 +0000

    Fri, Oct 10, 2025, 11:08 PM +00:00

--- flutter/flutter_mapp/lib/views/pages/settings_page.dart
+++ flutter/flutter_mapp/lib/views/pages/settings_page.dart
@@ -17,138 +17,140 @@ class _SettingsPageState extends State<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DropdownButton(
-              hint: Text('Hint'),
-              value: menuItem,
-              items: [
-                DropdownMenuItem(
-                  value: 'e1',
-                  child: Text('Element 1'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DropdownButton(
+                hint: Text('Hint'),
+                value: menuItem,
+                items: [
+                  DropdownMenuItem(
+                    value: 'e1',
+                    child: Text('Element 1'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'e2',
+                    child: Text('Element 2'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'e3',
+                    child: Text('Element 3'),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    menuItem = value;
+                  });
+                },
+              ),
+              TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
                 ),
-                DropdownMenuItem(
-                  value: 'e2',
-                  child: Text('Element 2'),
+                onEditingComplete: () {
+                  setState(() {});
+                },
+              ),
+              Text(controller.text),
+              Checkbox(
+                tristate: true,
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value;
+                  });
+                },
+              ),
+              CheckboxListTile.adaptive(
+                tristate: true,
+                title: Text('Click me'),
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value;
+                  });
+                },
+              ),
+              Switch(
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+              SwitchListTile.adaptive(
+                title: Text('Switch me'),
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+              Slider(
+                max: 10.0,
+                value: sliderValue,
+                divisions: 10,
+                label: '$sliderValue',
+                onChanged: (double value) {
+                  setState(() {
+                    sliderValue = value;
+                  });
+                },
+              ),
+              GestureDetector(
+                child: Image.asset(
+                  'assets/images/background.jpg',
                 ),
-                DropdownMenuItem(
-                  value: 'e3',
-                  child: Text('Element 3'),
+                onTap: () {
+                  print('tap');
+                },
+              ),
+              InkWell(
+                splashColor: Colors.teal,
+                onTap: () {
+                  print('container tapped');
+                },
+                child: Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Colors.white12,
                 ),
-              ],
-              onChanged: (value) {
-                setState(() {
-                  menuItem = value;
-                });
-              },
-            ),
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
               ),
-              onEditingComplete: () {
-                setState(() {});
-              },
-            ),
-            Text(controller.text),
-            Checkbox(
-              tristate: true,
-              value: isChecked,
-              onChanged: (bool? value) {
-                setState(() {
-                  isChecked = value;
-                });
-              },
-            ),
-            CheckboxListTile.adaptive(
-              tristate: true,
-              title: Text('Click me'),
-              value: isChecked,
-              onChanged: (bool? value) {
-                setState(() {
-                  isChecked = value;
-                });
-              },
-            ),
-            Switch(
-              value: isSwitched,
-              onChanged: (bool value) {
-                setState(() {
-                  isSwitched = value;
-                });
-              },
-            ),
-            SwitchListTile.adaptive(
-              title: Text('Switch me'),
-              value: isSwitched,
-              onChanged: (bool value) {
-                setState(() {
-                  isSwitched = value;
-                });
-              },
-            ),
-            Slider(
-              max: 10.0,
-              value: sliderValue,
-              divisions: 10,
-              label: '$sliderValue',
-              onChanged: (double value) {
-                setState(() {
-                  sliderValue = value;
-                });
-              },
-            ),
-            GestureDetector(
-              child: Image.asset(
-                'assets/images/background.jpg',
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text('Click me'),
               ),
-              onTap: () {
-                print('tap');
-              },
-            ),
-            InkWell(
-              splashColor: Colors.teal,
-              onTap: () {
-                print('container tapped');
-              },
-              child: Container(
-                height: 100,
-                width: double.infinity,
-                color: Colors.white12,
+              BackButton(
+                onPressed: () {},
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
-                foregroundColor: Colors.white,
+              CloseButton(
+                onPressed: () {},
               ),
-              child: Text('Click me'),
-            ),
-            BackButton(
-              onPressed: () {},
-            ),
-            CloseButton(
-              onPressed: () {},
-            ),
-            FilledButton(
-              onPressed: () {},
-              child: Text('Click me'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Click me'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('Click me'),
-            ),
-          ],
+              FilledButton(
+                onPressed: () {},
+                child: Text('Click me'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('Click me'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('Click me'),
+              ),
+            ],
+          ),
         ),
       ),
     );
