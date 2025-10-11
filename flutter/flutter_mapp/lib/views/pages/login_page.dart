2e0130ecb00eb987061c7509d6e94061bd61cb00
mdeyanov83 <mdeyanov@gmail.com>
2025-10-11 22:26:41 +0000

    Sat, Oct 11, 2025, 10:26 PM +00:00

--- flutter/flutter_mapp/lib/views/pages/login_page.dart
+++ flutter/flutter_mapp/lib/views/pages/login_page.dart
@@ -26,57 +26,60 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            HeroWidget(
-              title: 'Login',
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            TextField(
-              controller: controllerEmail,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                HeroWidget(
+                  title: 'Login',
                 ),
-              ),
-              onEditingComplete: () {
-                setState(() {});
-              },
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            TextField(
-              controller: controllerPw,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
+                SizedBox(
+                  height: 20.0,
                 ),
-              ),
-              onEditingComplete: () {
-                setState(() {});
-              },
+                TextField(
+                  controller: controllerEmail,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                  controller: controllerPw,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    onLoginPressed();
+                  },
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text('Login'),
+                ),
+              ],
             ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                onLoginPressed();
-              },
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text('Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
