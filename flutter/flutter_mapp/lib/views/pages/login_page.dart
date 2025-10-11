06b6e4eacd261537c4752cdb8954baeec0d5e04a
mdeyanov83 <mdeyanov@gmail.com>
2025-10-11 21:44:23 +0000

    Sat, Oct 11, 2025, 9:44 PM +00:00

--- flutter/flutter_mapp/lib/views/pages/login_page.dart
+++ flutter/flutter_mapp/lib/views/pages/login_page.dart
@@ -62,7 +62,7 @@ class _LoginPageState extends State<LoginPage> {
               },
             ),
             SizedBox(height: 20.0),
-            FilledButton(
+            ElevatedButton(
               onPressed: () {
                 Navigator.pushReplacement(
                   context,
