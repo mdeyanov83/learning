f31191f87f39615cef3520cd53c5af60f91bd588
mdeyanov83 <mdeyanov@gmail.com>
2025-10-12 12:31:57 +0000

    Sun, Oct 12, 2025, 12:31 PM +00:00

--- flutter/flutter_mapp/lib/main.dart
+++ flutter/flutter_mapp/lib/main.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mapp/data/constants.dart';
 import 'package:flutter_mapp/data/notifiers.dart';
 import 'package:flutter_mapp/views/pages/welcome_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    initThemeMode();
+    super.initState();
+  }
+
+  void initThemeMode() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool? repeat = prefs.getBool(KConstants.themeModeKey);
+    isDarkModeNotifier.value = repeat ?? false;
+  }
 
   @override
   Widget build(BuildContext context) {
