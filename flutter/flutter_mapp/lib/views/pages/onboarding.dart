0721ca964f82eb97b1e6a00f7611a2c378c47a2d
mdeyanov83 <mdeyanov@gmail.com>
2025-10-12 20:03:44 +0000

    Sun, Oct 12, 2025, 8:03 PM +00:00

---
+++ flutter/flutter_mapp/lib/views/pages/onboarding.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mapp/views/widget_tree.dart';
+import 'package:lottie/lottie.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController controllerEmail = TextEditingController(text: '123');
+  TextEditingController controllerPw = TextEditingController(text: '456');
+
+  String confirmedEmail = '123';
+  String confirmedPassword = '456';
+
+  @override
+  void dispose() {
+    controllerEmail.dispose();
+    controllerPw.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/lotties/home.json', height: 400.0),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  controller: controllerEmail,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                  controller: controllerPw,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(height: 20.0),
+                FilledButton(
+                  onPressed: () {
+                    onLoginPressed();
+                  },
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text(widget.title),
+                ),
+                SizedBox(height: 50.0),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onLoginPressed() {
+    if (confirmedEmail == controllerEmail.text &&
+        confirmedPassword == controllerPw.text) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return WidgetTree();
+          },
+        ),
+        (route) => false,
+      );
+    }
+  }
+}
