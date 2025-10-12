5763a2896c7a7657dadae3f845de19d977c39015
mdeyanov83 <mdeyanov@gmail.com>
2025-10-12 19:10:02 +0000

    Sun, Oct 12, 2025, 7:10 PM +00:00

---
+++ flutter/flutter_mapp/course_examples/lib_9_before_ui_mod4/views/widget_tree.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mapp/data/constants.dart';
+import 'package:flutter_mapp/data/notifiers.dart';
+import 'package:flutter_mapp/views/pages/home_page.dart';
+import 'package:flutter_mapp/views/pages/profile_page.dart';
+import 'package:flutter_mapp/views/pages/settings_page.dart';
+import 'package:flutter_mapp/views/widgets/navbar_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+List<Widget> pages = [
+  HomePage(),
+  ProfilePage(),
+];
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Mapp'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              isDarkModeNotifier.value = !isDarkModeNotifier.value;
+              final SharedPreferences prefs =
+                  await SharedPreferences.getInstance();
+              await prefs.setBool(KConstants.themeModeKey, isDarkModeNotifier.value);
+            },
+            icon: ValueListenableBuilder(
+              valueListenable: isDarkModeNotifier,
+              builder: (context, isDarkMode, child) {
+                return Icon(
+                  isDarkMode ? Icons.light_mode : Icons.dark_mode,
+                );
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SettingsPage(
+                      title: 'Settings page',
+                    );
+                  },
+                ),
+              );
+            },
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
+      bottomNavigationBar: NavBarWidget(),
+    );
+  }
+}
---
+++ flutter/flutter_mapp/course_examples/lib_9_before_ui_mod4/views/widgets/hero_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HeroWidget extends StatelessWidget {
+  const HeroWidget({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Hero(
+          tag: 'hero1',
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.asset(
+              'assets/images/background.jpg',
+              color: Colors.teal,
+              colorBlendMode: BlendMode.darken,
+            ),
+          ),
+        ),
+        FittedBox(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 50.0,
+              letterSpacing: 50.0,
+              color: Colors.white30,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
