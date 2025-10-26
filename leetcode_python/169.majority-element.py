1364ce6e2aaa5157822c439b5dd78878877ec805
mdeyanov83 <mdeyanov@gmail.com>
2025-10-26 21:18:17 +0000

    Sun, Oct 26, 2025, 9:18 PM +00:00

---
+++ leetcode_python/169.majority-element.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=169 lang=python3
+#
+# [169] Majority Element
+#
+
+from typing import List
+
+# @lc code=start
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+# @lc code=end
+
+def main():
+    sol = Solution()
+
+    
+
+
+if __name__ == "__main__":
+    main()
