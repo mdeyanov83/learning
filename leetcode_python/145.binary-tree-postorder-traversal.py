be4e693c5275f071b4abf826bf71f5c43cf09f99
mdeyanov83 <mdeyanov@gmail.com>
2025-10-26 15:23:12 +0000

    Sun, Oct 26, 2025, 3:23 PM +00:00

---
+++ leetcode_python/145.binary-tree-postorder-traversal.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=145 lang=python3
+#
+# [145] Binary Tree Postorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import Optional, List
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        path = []
+
+        def walk(node: Optional[TreeNode]) -> List[int]:
+            if not node:
+                return
+
+            walk(node.left)
+            walk(node.right)
+            path.append(node)
+
+        walk(root)
+
+        return path
+
+# @lc code=end
+
