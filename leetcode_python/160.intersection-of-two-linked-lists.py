771686bd10b87b2b96f49ddde56f773f20be7628
mdeyanov83 <mdeyanov@gmail.com>
2025-10-26 15:40:15 +0000

    Sun, Oct 26, 2025, 3:40 PM +00:00

---
+++ leetcode_python/160.intersection-of-two-linked-lists.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=160 lang=python3
+#
+# [160] Intersection of Two Linked Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+from typing import Optional
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+
+# @lc code=end
+
