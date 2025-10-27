c9c2b85170142a3ec561343f18eb4fa2893400ae
mdeyanov83 <mdeyanov@gmail.com>
2025-10-27 00:37:35 +0000

    Mon, Oct 27, 2025, 12:37 AM +00:00

---
+++ leetcode_python/175.combine-two-tables.sql
@@ -0,0 +1,13 @@
+--
+-- @lc app=leetcode id=175 lang=mysql
+--
+-- [175] Combine Two Tables
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+SELECT * FROM "Person table" JOIN "Address table" on personID = "Address table".personID
+
+-- @lc code=end
+
