64d8f8e87c8a265c8b4b0f3b013a71fe2064d56e
mdeyanov83 <mdeyanov@gmail.com>
2025-10-27 11:32:22 +0000

    Mon, Oct 27, 2025, 11:32 AM +00:00

--- leetcode_python/181.employees-earning-more-than-their-managers.sql
+++ leetcode_python/181.employees-earning-more-than-their-managers.sql
@@ -8,7 +8,7 @@
 # Write your MySQL query statement below
 
 SELECT name FROM Employee WHERE salary >
-    SELECT salary FROM Employee JOIN Emplo
+    SELECT salary FROM Employee JOIN 
 
 
 -- @lc code=end
