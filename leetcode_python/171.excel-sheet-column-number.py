8d7d7b44e39a53b68040247ad9da82a63ee1829a
mdeyanov83 <mdeyanov@gmail.com>
2025-10-27 00:13:13 +0000

    Mon, Oct 27, 2025, 12:13 AM +00:00

---
+++ leetcode_python/171.excel-sheet-column-number.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=171 lang=python3
+#
+# [171] Excel Sheet Column Number
+#
+
+# @lc code=start
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        values = {'A': 1,
+                  'B': 2,}
+
+        return 1
+
+# @lc code=end
+
+def main():
+    sol = Solution()
+    input = 'A'
+    print(sol.titleToNumber(input))
+
+if __name__ == "__main__":
+    main()
+
