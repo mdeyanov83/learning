1c10ec5c78a1834a86514f5787cfe164ec22ab5e
mdeyanov83 <mdeyanov@gmail.com>
2025-10-29 02:04:16 +0000

    Wed, Oct 29, 2025, 2:04 AM +00:00

---
+++ leetcode_python/182.duplicate-emails.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode id=182 lang=mysql
+--
+-- [182] Duplicate Emails
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+select email as Email
+from Person
+group by email
+having count(email) > 1
+
+-- @lc code=end
+
