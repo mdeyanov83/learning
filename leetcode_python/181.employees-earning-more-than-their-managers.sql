97c283577f61511d7417f0353ee1d824898abe55
mdeyanov83 <mdeyanov@gmail.com>
2025-10-29 02:08:09 +0000

    Wed, Oct 29, 2025, 2:08 AM +00:00

--- leetcode_python/181.employees-earning-more-than-their-managers.sql
+++ leetcode_python/181.employees-earning-more-than-their-managers.sql
@@ -7,7 +7,13 @@
 -- @lc code=start
 # Write your MySQL query statement below
 
+/*************  ✨ Windsurf Command 🌟  *************/
+SELECT e.Name AS Employee
+FROM Employee e
+INNER JOIN Employee m ON e.ManagerId = m.Id
+WHERE e.Salary > m.Salary
 SELECT e.name AS Employee from Employee e JOIN Employee m WHERE e.managerId = m.id AND e.salary > m.salary
+/*******  e0b4b189-3cc9-4f8d-a0aa-1fe1bf162c06  *******/
 
 
 -- @lc code=end
