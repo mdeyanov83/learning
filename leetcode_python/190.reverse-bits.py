2eb49580e7c548b19165d23899b80fc5955f75a3
mdeyanov83 <mdeyanov@gmail.com>
2025-10-31 20:10:55 +0000

    Fri, Oct 31, 2025, 8:10 PM +00:00

--- leetcode_python/190.reverse-bits.py
+++ leetcode_python/190.reverse-bits.py
@@ -5,22 +5,22 @@
 #
 
 # @lc code=start
-# class Solution:
-#     def reverseBits(self, n: int) -> int:
-#         bin = f'{n:032b}'
-#         bin_reversed = bin[::-1]
-#         return int(bin_reversed, 2)
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        bin = f'{n:032b}'
+        bin_reversed = bin[::-1]
+        return int(bin_reversed, 2)
 
 
 # Using bit manipulation
-class Solution:
-    def reverseBits(self, n):
-        result = 0
-        for _ in range(32):
-            result <<= 1        # Shift result left to make space for next bit (places 0 as last bit)
-            result |= n & 1     # Copy the last bit of n into result (last bit becomes 0 OR last bit of n)
-            n >>= 1             # Shift n right and discard least significant bit
-        return result
+# class Solution:
+#     def reverseBits(self, n):
+#         result = 0
+#         for _ in range(32):
+#             result <<= 1        # Shift result left to make space for next bit (places 0 as last bit)
+#             result |= n & 1     # Copy the last bit of n into result (last bit becomes 0 OR last bit of n)
+#             n >>= 1             # Shift n right and discard least significant bit
+#         return result
 
 
 # @lc code=end
