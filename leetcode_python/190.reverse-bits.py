e28cc46c8b633492135d22b06f4abaf9a5d2db55
mdeyanov83 <mdeyanov@gmail.com>
2025-10-31 19:25:58 +0000

    Fri, Oct 31, 2025, 7:25 PM +00:00

--- leetcode_python/190.reverse-bits.py
+++ leetcode_python/190.reverse-bits.py
@@ -7,8 +7,9 @@
 # @lc code=start
 class Solution:
     def reverseBits(self, n: int) -> int:
-        bin = f'{n:032b}'
-        bin_reversed = bin[::-1]
-        return int(bin_reversed, 2)
+        # bin = f'{n:032b}'
+        # bin_reversed = bin[::-1]
+        # return int(bin_reversed, 2)
+        return int(f'{n:032b}'[::-1])
 
 # @lc code=end
