3eae19c414ea365b4257b7e3bc45648d5f87d6f1
mdeyanov83 <mdeyanov@gmail.com>
2025-10-31 20:25:32 +0000

    Fri, Oct 31, 2025, 8:25 PM +00:00

---
+++ leetcode_python/191.number-of-1-bits.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=191 lang=python
+#
+# [191] Number of 1 Bits
+#
+
+# @lc code=start
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+# @lc code=end
+
