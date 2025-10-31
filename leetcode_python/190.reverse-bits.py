3e485e876bdae8b95387f53a077a06f1be41d0e3
mdeyanov83 <mdeyanov@gmail.com>
2025-10-31 20:07:48 +0000

    Fri, Oct 31, 2025, 8:07 PM +00:00

--- leetcode_python/190.reverse-bits.py
+++ leetcode_python/190.reverse-bits.py
@@ -17,8 +17,8 @@ class Solution:
     def reverseBits(self, n):
         result = 0
         for _ in range(32):
-            result <<= 1        # Shift result left to make space for next bit
-            result |= n & 1     # Copy the last bit of n into result
+            result <<= 1        # Shift result left to make space for next bit (places 0 as last bit)
+            result |= n & 1     # Copy the last bit of n into result (last bit becomes 0 OR last bit of n)
             n >>= 1             # Shift n right and discard least significant bit
         return result
 
