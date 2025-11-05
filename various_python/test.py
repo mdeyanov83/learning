ff666d385d55cf61ad632df5e433774317bf4cc3
mdeyanov83 <mdeyanov@gmail.com>
2025-11-05 14:14:01 +0000

    Wed, Nov 5, 2025, 2:14 PM +00:00

--- various_python/test.py
+++ various_python/test.py
@@ -8,9 +8,9 @@ nums = [1, 1, 2, 2, 1, 3, 3, 4, 5, 1, 2, 3, 3]
 str = 'aabbbcaaccjjjseeccabbbccbbbbbbxxx'
 count = Counter(str)
 
-dict = {'a': 1, 'b': 2, 'c': 6, 'd': 3}
-print(dict.values())
 print(count.values())
 
+sort = sorted(count, key=count.values())
 
-print(count.items())
+
+print(sort)
