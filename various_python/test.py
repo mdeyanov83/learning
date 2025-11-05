12a8e1a8e97f1d90ffabdfff9b161711cca51abe
mdeyanov83 <mdeyanov@gmail.com>
2025-11-05 14:12:15 +0000

    Wed, Nov 5, 2025, 2:12 PM +00:00

--- various_python/test.py
+++ various_python/test.py
@@ -5,4 +5,12 @@ from collections import Counter
 
 nums = [1, 1, 2, 2, 1, 3, 3, 4, 5, 1, 2, 3, 3]
 
-counter = Counter(nums)
+str = 'aabbbcaaccjjjseeccabbbccbbbbbbxxx'
+count = Counter(str)
+
+dict = {'a': 1, 'b': 2, 'c': 6, 'd': 3}
+print(dict.values())
+print(count.values())
+
+
+print(count.items())
