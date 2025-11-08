48650a1b449b7d082265df0ef5baf10601437a43
mdeyanov83 <mdeyanov@gmail.com>
2025-11-08 19:24:17 +0000

    Sat, Nov 8, 2025, 7:24 PM +00:00

--- FastAPI_tutorial/app/app.py
+++ FastAPI_tutorial/app/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-text_posts = {"1": {"title": "New Post", "content": "cool test post"}}
+text_posts = {1: {"title": "New Post", "content": "cool test post"}}
 
 @app.get("/posts")
 def get_all_posts():
