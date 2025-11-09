c25d350cae629a934526b098c180b18654222e94
mdeyanov83 <mdeyanov@gmail.com>
2025-11-09 02:17:21 +0000

    Sun, Nov 9, 2025, 2:17 AM +00:00

--- FastAPI_tutorial/app/db.py
+++ FastAPI_tutorial/app/db.py
@@ -8,3 +8,9 @@ from sqlalchemy.orm import DeclarativeBase, relationship
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
+class Post(DeclarativeBase):
+    __tablename__ = "posts"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    
+
