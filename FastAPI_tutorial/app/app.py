c9e8355e6a9f31bf1930eb9ec8662a30c0553dcb
mdeyanov83 <mdeyanov@gmail.com>
2025-11-09 12:28:28 +0000

    Sun, Nov 9, 2025, 12:28 PM +00:00

--- FastAPI_tutorial/app/app.py
+++ FastAPI_tutorial/app/app.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas import PostCreate, PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
 
-
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 text_posts = {
     1: {"title": "New Post", "content": "Exploring the wonders of AI."},
--- FastAPI_tutorial/app/db.py
+++ FastAPI_tutorial/app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
-import datetime
+from datetime import datetime
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
