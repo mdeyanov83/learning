a851fe728ea023e6c7f3fd08643a15d8ba907d95
mdeyanov83 <mdeyanov@gmail.com>
2025-11-09 02:12:46 +0000

    Sun, Nov 9, 2025, 2:12 AM +00:00

--- FastAPI_tutorial/app/db.py
+++ FastAPI_tutorial/app/db.py
@@ -2,4 +2,9 @@ from collections.abc import AsyncGenerator
 import uuid
 
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, relationship
+
+
 
