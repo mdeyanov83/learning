9904557368c974b286da346d0d16b171b1894faa
mdeyanov83 <mdeyanov@gmail.com>
2025-11-09 18:49:41 +0000

    Sun, Nov 9, 2025, 6:49 PM +00:00

--- FastAPI_tutorial/app/app.py
+++ FastAPI_tutorial/app/app.py
@@ -89,6 +89,21 @@ async def get_feed(
 
 @app.delete("/posts/{post_id}")
 async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session)):
+    try:
+        post_uuid = uuid.UUID(post_id)
+
+        result = await session.execute(select(Post).where(Post.id == post_uuid))
+        post = result.scalars().first()
+
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+
+        await session.delete(post)
+        await sessio.commit()
+
+        return {"success": True, "messege": "Post delete successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 
