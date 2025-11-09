b9d6682ec954fef130ab9bf9cabc5c52f750d27c
mdeyanov83 <mdeyanov@gmail.com>
2025-11-09 19:15:19 +0000

    Sun, Nov 9, 2025, 7:15 PM +00:00

--- FastAPI_tutorial/app/users.py
+++ FastAPI_tutorial/app/users.py
@@ -9,3 +9,11 @@ from fastapi_users.authentication import (
 )
 from fastapi_users.db import SQLAlchemyUserDatabase
 from app.db import User, get_user_db
+
+
+SECRET = "abc123abc123"
+
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
