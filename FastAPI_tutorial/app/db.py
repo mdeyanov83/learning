155a8bcf62e0bbb09cea90709d58124aaa1b628f
mdeyanov83 <mdeyanov@gmail.com>
2025-11-09 02:14:33 +0000

    Sun, Nov 9, 2025, 2:14 AM +00:00

--- FastAPI_tutorial/app/db.py
+++ FastAPI_tutorial/app/db.py
@@ -3,8 +3,8 @@ import uuid
 
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, relationship
 
-
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
