c873c42593a9c87ac1bd9a18ab605bb3efb3373c
mdeyanov83 <mdeyanov@gmail.com>
2025-11-11 12:56:24 +0000

    Tue, Nov 11, 2025, 12:56 PM +00:00

--- LeetCode/474_ones_and_zeroes.py
+++ LeetCode/474_ones_and_zeroes.py
@@ -1,39 +1,46 @@
 import itertools
 from typing import List
 
+# class Solution:
+#     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+#         def is_valid(k):
+#             for subset in itertools.combinations(counts, k):
+#                 sum0, sum1 = 0, 0
+#                 for el in subset:
+#                     sum0 += counts[el][0]
+#                     sum1 += counts[el][1]
+#                 if (sum0 <= m) and (sum1 <= n):
+#                     return True
+#             return False
+
+#         counts = dict()
+#         length = len(strs)
+
+#         for i in range(length):
+#             ones = strs[i].count('1')
+#             zeroes = len(strs[i]) - ones
+#             counts[i] = {
+#                 0: zeroes,
+#                 1: ones
+#             }
+
+#         low = 0
+#         high = length
+
+#         while low < high:
+#             mid = (low + high + 1) // 2
+#             if is_valid(mid):
+#                 low = mid
+#             else:
+#                 high = mid - 1
+#         return low
+
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        def is_valid(k):
-            for subset in itertools.combinations(counts, k):
-                sum0, sum1 = 0, 0
-                for el in subset:
-                    sum0 += counts[el][0]
-                    sum1 += counts[el][1]
-                if (sum0 <= m) and (sum1 <= n):
-                    return True
-            return False
 
-        counts = dict()
-        length = len(strs)
-
-        for i in range(length):
-            ones = strs[i].count('1')
-            zeroes = len(strs[i]) - ones
-            counts[i] = {
-                0: zeroes,
-                1: ones
-            }
-
-        low = 0
-        high = length
-
-        while low < high:
-            mid = (low + high + 1) // 2
-            if is_valid(mid):
-                low = mid
-            else:
-                high = mid - 1
-        return low
+        # Initialize DP array
+        dp[i][j] = 0 for all i in [0..m], j in [0..n]
+            
 
 
 def main():
