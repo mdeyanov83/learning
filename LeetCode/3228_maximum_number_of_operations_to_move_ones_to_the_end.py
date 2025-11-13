0799f863d3e6b3ff4d3b81d48534a54ba0d6d36f
mdeyanov83 <mdeyanov@gmail.com>
2025-11-13 02:50:40 +0000

    Thu, Nov 13, 2025, 2:50 AM +00:00

--- LeetCode/3228_maximum_number_of_operations_to_move_ones_to_the_end.py
+++ LeetCode/3228_maximum_number_of_operations_to_move_ones_to_the_end.py
@@ -6,16 +6,16 @@ class Solution:
         lst = list(s)
         n = len(lst)
         count = 0
-        start = 0
+        # start = 0
         end = n-1
 
         while True:
             acted = False
-            started = False
-            for i in range(start, end):
-                if lst[i] == '1' and not started:
-                    start = i
-                    started = True
+            # started = False
+            for i in range(lst.index('1'), end):
+                # if lst[i] == '1' and not started:
+                #     start = i
+                #     started = True
                 if lst[i] == '1' and lst[i+1] == '0':
 
                     # Walk the list from current "1" to last trailing 1 or end of list and swap.
@@ -26,7 +26,7 @@ class Solution:
                             count += 1
                             acted = True
                             break
-                        
+
             if not acted:
                 break
 
