77ee753c8276e46823db8ca71fedcf584555eeab
mdeyanov83 <mdeyanov@gmail.com>
2025-11-17 21:43:28 +0000

    Mon, Nov 17, 2025, 9:43 PM +00:00

--- python_projects/p3_rock_paper_scissors.py
+++ python_projects/p3_rock_paper_scissors.py
@@ -3,10 +3,19 @@ import random
 user_wins = 0
 computer_wins = 0
 
+options = ["rock", "paper", "scissors"]
+
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == 'q':
-        quit()
+        break
 
-    if user_input not  in ["rock", "paper", "scissors"]:
+    if user_input not  in options:
         continue
+
+    random.number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+
+
+
+print("Goodbye!")
