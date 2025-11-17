2cb50f40b23a9c673bde6831bdeeaf3255f6ef64
mdeyanov83 <mdeyanov@gmail.com>
2025-11-17 22:08:14 +0000

    Mon, Nov 17, 2025, 10:08 PM +00:00

--- python_projects/p4_choose_your_own_adventure.py
+++ python_projects/p4_choose_your_own_adventure.py
@@ -3,12 +3,12 @@ print(f"Welcome {name} to this adventure!")
 
 
 answer = input(
-    "You are on a dirt road, it has come to an end and you can go left or right. Which way would youl like to go?: "
+    "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? "
     ).lower()
 
 if answer == "left":
     answer = input(
-        "You come to a river, you can walk around it or swim across? Type walk to walk around or swim to swim around?: "
+        "You come to a river, you can walk around it or swim across? Type walk to walk around or swim to swim around?  "
         ).lower()
     if answer == "swim":
         print("You swam across and were eaten by an alligator.")
@@ -18,6 +18,12 @@ if answer == "left":
         print("Not a valid option. You lose.")
 
 elif answer == "right":
-    print()
+    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ")
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        answer == input("You cross the bridge and meet a stranger. Do you talk to them? (Yes, No)")
+    else:
+        print("Not a valid option. You lose.")
 else:
     print("Not a valid option. You lose.")
