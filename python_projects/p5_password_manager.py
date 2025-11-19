09a9086df4de00cbdbcea5ea19befa6cefc7150e
mdeyanov83 <mdeyanov@gmail.com>
2025-11-19 16:03:08 +0000

    Wed, Nov 19, 2025, 4:03 PM +00:00

--- python_projects/p5_password_manager.py
+++ python_projects/p5_password_manager.py
@@ -2,11 +2,12 @@ from cryptography.fernet import Fernet
 
 # def write_key():
 #     key = Fernet.generate_key()
-#     with open("key.key", "wb") as key_file:
+#     with open("p5_key.key", "wb") as key_file:
 #         key_file.write(key)
 
+
 def load_key():
-    file = open("key.key", "rb")
+    file = open("p5_key.key", "rb")
     key = file.read()
     file.close()
     return key
@@ -15,16 +16,20 @@ def load_key():
 # To use a master password:
 # https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
 
+
 key = load_key()
 print(key)
 fer = Fernet(key)
+
 
 def view():
     with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print(f"User: {user}| Password: {fer.decrypt(passw.encode()).decode()}")
+            print(
+                f"User: {user}| Password: {fer.decrypt(passw.encode()).decode()}")
+
 
 def add():
     name = input("Account Name: ")
@@ -37,7 +42,7 @@ def add():
 while True:
     mode = input(
         "Would you like to add a new password or view existing ones (view, add), press Q to quit? "
-        ).lower()
+    ).lower()
     if mode == "q":
         break
 
