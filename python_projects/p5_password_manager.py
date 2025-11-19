e2b2adec7d94bb63fc8ccb5345b2aac4fe956386
mdeyanov83 <mdeyanov@gmail.com>
2025-11-19 15:49:27 +0000

    Wed, Nov 19, 2025, 3:49 PM +00:00

--- python_projects/p5_password_manager.py
+++ python_projects/p5_password_manager.py
@@ -1,11 +1,19 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
+# def write_key():
+#     key = Fernet.generate_key()
+#     with open("key.key", "wb") as key_file:
+#         key_file.write(key)
 
-def write_key():
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input("What is the master password? ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
     with open("passwords.txt", "r") as f:
@@ -19,7 +27,7 @@ def add():
     pwd = input("password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(f"{name}|{pwd}\n")
+        f.write(f"{name}|{fer.encrypt(pwd.encode())}\n")
 
 
 while True:
