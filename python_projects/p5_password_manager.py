a6c3a5ef3c6a38336ea64106c5bda0cb5d512f22
mdeyanov83 <mdeyanov@gmail.com>
2025-11-19 14:02:15 +0000

    Wed, Nov 19, 2025, 2:02 PM +00:00

--- python_projects/p5_password_manager.py
+++ python_projects/p5_password_manager.py
@@ -1,3 +1,20 @@
 pwd = input("What is the master password? ")
 
+def view():
+    pass
 
+def add():
+    pass
+
+while True:
+
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press Q to quit? "
+        ).lower()
+    if mode == "view":
+        ...
+    elif mode == "add":
+        ...
+    else:
+        print("Invalid mode.")
+        continue
