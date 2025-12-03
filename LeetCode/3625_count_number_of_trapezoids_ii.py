a575e401eead9ca50dba726777c418bf1c1af05c
mdeyanov83 <mdeyanov@gmail.com>
2025-12-03 20:15:28 +0000

    Wed, Dec 3, 2025, 8:15 PM +00:00

--- LeetCode/3625_count_number_of_trapezoids_ii.py
+++ LeetCode/3625_count_number_of_trapezoids_ii.py
@@ -44,6 +44,7 @@ class Solution:
                 midpoints[(mid_x, mid_y)] += 1
 
         for bucket in buckets.values():
+             print(bucket)
              for (a, b), (c, d) in combinations(bucket, 2):
                 print(a, b, c, d, '->', len({a, b, c, d}))
 
