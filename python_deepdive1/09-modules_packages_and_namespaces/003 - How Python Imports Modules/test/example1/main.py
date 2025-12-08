07092f529179a411b66a48bccc523644776e9c69
mdeyanov83 <mdeyanov@gmail.com>
2025-12-08 14:30:43 +0000

    Mon, Dec 8, 2025, 2:30 PM +00:00

--- python_deepdive1/09-modules_packages_and_namespaces/003 - How Python Imports Modules/test/example1/main.py
+++ python_deepdive1/09-modules_packages_and_namespaces/003 - How Python Imports Modules/test/example1/main.py
@@ -3,5 +3,13 @@ print('========================================')
 print(f'Running main.py - module name: {__name__}')
 
 import module1
+import sys
+
+
+print(module1)
+
+module1.pprint_dict('main.globals', globals())
+
+
 
 print('========================================')
