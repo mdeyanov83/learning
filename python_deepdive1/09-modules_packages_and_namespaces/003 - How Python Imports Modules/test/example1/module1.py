8d3d2e8a58850ebd50cb7330e64edb8b9f804446
mdeyanov83 <mdeyanov@gmail.com>
2025-12-08 14:06:04 +0000

    Mon, Dec 8, 2025, 2:06 PM +00:00

--- python_deepdive1/09-modules_packages_and_namespaces/003 - How Python Imports Modules/test/example1/module1.py
+++ python_deepdive1/09-modules_packages_and_namespaces/003 - How Python Imports Modules/test/example1/module1.py
@@ -1,10 +1,12 @@
 print(f'------------- Running {__name__} ----------------')
 
 def pprint_dict(header, d):
-
-
     print('\n\n---------------------------------------------')
-    print()
+    print(f'******** {header} ********')
+    for key, value in d.items():
+        print(key, value)
+    print('---------------------------------------------\n\n')
 
+pprint_dict('module1.globals', globals())
 
-
+print(f'---------------- End of {__name__} -----------------')
