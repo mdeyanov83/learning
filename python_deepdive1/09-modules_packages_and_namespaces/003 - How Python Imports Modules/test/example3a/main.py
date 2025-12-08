73bc2f9542a63d30b782ee8843295659818ca774
mdeyanov83 <mdeyanov@gmail.com>
2025-12-08 14:52:06 +0000

    Mon, Dec 8, 2025, 2:52 PM +00:00

--- python_deepdive1/09-modules_packages_and_namespaces/003 - How Python Imports Modules/test/example3a/main.py
+++ python_deepdive1/09-modules_packages_and_namespaces/003 - How Python Imports Modules/test/example3a/main.py
@@ -9,7 +9,7 @@ module_file = 'module1_source.py'
 module_path = '.'
 
 module_rel_file_path = os.path.join(module_path, module_file)
-module_abs_file_path = ps.path.abspath(module_rel_file_path)
+module_abs_file_path = os.path.abspath(module_rel_file_path)
 
 # read soruce code from file
 with open(module_rel_file_path, 'r') as code_file:
@@ -27,3 +27,7 @@ code = compile(source_code, filename=module_abs_file_path, mode='exec')
 
 # execute the compiled source code
 exec(code, mod.__dict__)
+
+# DONE!
+
+mod.hello()
