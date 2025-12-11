130791449cb1b93e6dce02efae0dfd1dfe696dc2
mdeyanov83 <mdeyanov@gmail.com>
2025-12-11 00:50:20 +0000

    Thu, Dec 11, 2025, 12:50 AM +00:00

--- python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/main.py
+++ python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/main.py
@@ -1,11 +1,14 @@
 import common.validators.boolean
 import common.validators.date
 import common.validators.json
-import common.validators.numeric
+# import common.validators.numeric
+from common.validators.numeric import is_integer, is_numeric
 
 
 common.validators.json.is_json('{}')
 common.validators.date.is_date('2018-01-01')
+is_numeric(10)
+is_integer(100)
 
 
 print('\n\n****** self ******')
@@ -15,4 +18,14 @@ for k in dict(globals()).keys():
 
 print('\n\n****** common ******')
 for k in common.__dict__.keys():
+    print(k)
+
+
+print('\n\n****** validators ******')
+for k in common.validators.__dict__.keys():
+    print(k)
+
+
+print('\n\n****** numeric ******')
+for k in common.validators.numeric.__dict__.keys():
     print(k)
