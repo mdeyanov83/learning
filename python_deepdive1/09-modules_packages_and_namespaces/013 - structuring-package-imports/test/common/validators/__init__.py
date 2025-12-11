66d4dc456b5afac42b070c349273a58c32fb68fb
mdeyanov83 <mdeyanov@gmail.com>
2025-12-11 01:43:28 +0000

    Thu, Dec 11, 2025, 1:43 AM +00:00

--- python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/common/validators/__init__.py
+++ python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/common/validators/__init__.py
@@ -10,4 +10,7 @@ from .date import *
 from .json import *
 from .numeric import *
 
-__all__ = boolean.__all__ + date.__all__ + json.__all__ + numeric.__all__
+__all__ = (boolean.__all__ +
+           date.__all__ +
+           json.__all__ +
+           numeric.__all__)
