68a906a869b70bca6fd0cdbf5efa1ec2e929d6d1
mdeyanov83 <mdeyanov@gmail.com>
2025-12-11 20:31:19 +0000

    Thu, Dec 11, 2025, 8:31 PM +00:00

--- python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/main.py
+++ python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/main.py
@@ -1,14 +1,17 @@
-import common.validators # as validators
-# import common # imported so we can see the common.__dict__ property
+import common # imported so we can see the common.__dict__ property
+import common.validators as validators
+import common.models as models
 
 
-
-# validators.is_boolean('True')
-# validators.is_json('{}')
-# validators.is_numeric(10)
-# validators.is_date('2008-01-01')
+validators.is_boolean('True')
+validators.is_json('{}')
+validators.is_numeric(10)
+validators.is_date('2008-01-01')
 
 # from common.validators import *
+
+
+john_post = common.models.posts.post.Post()
 
 
 print('\n\n****** self ******')
@@ -29,3 +32,8 @@ for k in common.__dict__.keys():
 # print('\n\n****** numeric ******')
 # for k in validators.numeric.__dict__.keys():
 #     print(k)
+
+
+print('\n\n****** models ******')
+for k in common.models.__dict__.keys():
+    print(k)
