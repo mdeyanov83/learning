0504bcee32742384b43b176d5c9cb8ff92d22a3c
mdeyanov83 <mdeyanov@gmail.com>
2025-12-11 00:44:19 +0000

    Thu, Dec 11, 2025, 12:44 AM +00:00

---
+++ python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/main.py
@@ -0,0 +1,13 @@
+import common.validators.boolean
+import common.validators.date
+import common.validators.json
+import common.validators.numeric
+
+
+common.validators.json.is_json('{}')
+common.validators.date.is_date('2018-01-01')
+
+
+print('\n\n****** self ******')
+for k in globals().keys():
+    print(k)
