37a51b0fba3ed262ca7628a55f8037169be8d959
mdeyanov83 <mdeyanov@gmail.com>
2025-12-11 01:05:08 +0000

    Thu, Dec 11, 2025, 1:05 AM +00:00

--- python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/shared/validators/__init__.py
+++ python_deepdive1/09-modules_packages_and_namespaces/013 - structuring-package-imports/test/shared/validators/__init__.py
@@ -5,7 +5,7 @@
 # import common.validators.json
 # import common.validators.numeric
 
-from common.validators.boolean import *
-from common.validators.date import *
-from common.validators.json import *
+from .boolean import *
+from .date import *
+from .json import *
 from common.validators.numeric import *
