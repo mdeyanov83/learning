cca799025a8de3c79f0064d1aebf9b6b76279160
mdeyanov83 <mdeyanov@gmail.com>
2025-12-13 17:23:31 +0000

    Sat, Dec 13, 2025, 5:23 PM +00:00

--- python_deepdive1/11-extras/008 - argparse/test/example5.py
+++ python_deepdive1/11-extras/008 - argparse/test/example5.py
@@ -9,5 +9,6 @@ print(args)
 
 first_name = args.get('--first-name', None)
 last_name = args.get('--last-name', None)
+yob = args.get('--yob', None)
 
-print(first_name, last_name)
+print(first_name, last_name, yob)
