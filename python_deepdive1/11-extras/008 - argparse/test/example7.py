696b959df26593f72eb9884847880f4bac476fe0
mdeyanov83 <mdeyanov@gmail.com>
2025-12-13 17:43:21 +0000

    Sat, Dec 13, 2025, 5:43 PM +00:00

--- python_deepdive1/11-extras/008 - argparse/test/example7.py
+++ python_deepdive1/11-extras/008 - argparse/test/example7.py
@@ -5,6 +5,20 @@ parser = argparse.ArgumentParser(description='Returns a string containing the na
 
 parser.add_argument('-f', '--first', help='first name', type=str, required=False, dest='first_name')
 parser.add_argument('-l', '--last', help='last name', type=str, required=True, dest='last_name')
-parser.add_argument('--yob', help='year of birt', type=int, required=False, dest='birth_year')
+parser.add_argument('--yob', help='year of birt', type=int, required=True, dest='birth_year')
+
 args = parser.parse_args()
+
+if args.first_name:
+    names = [args.first_name]
+else:
+    names = []
+
+names.append(args.last_name)
+full_name = ' '.join(names)
+
+current_year = datetime.datetime.now().year
+age = current_year - args.birth_year
+
+print(f'{full_name} is {age} years old.')
 
