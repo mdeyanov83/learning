768c439034321ee0c9b75cd4390e337276c52074
mdeyanov83 <mdeyanov@gmail.com>
2025-12-13 17:58:27 +0000

    Sat, Dec 13, 2025, 5:58 PM +00:00

--- python_deepdive1/11-extras/008 - argparse/test/example9.py
+++ python_deepdive1/11-extras/008 - argparse/test/example9.py
@@ -2,7 +2,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description='testing defaults and flags')
 
-parser.add_argument('--monty', action='store')
+parser.add_argument('--monty', action='store_const', const='Python')
+parser.add_argument('-n', '--name', default='John')
+
 
 args = parser.parse_args()
 
