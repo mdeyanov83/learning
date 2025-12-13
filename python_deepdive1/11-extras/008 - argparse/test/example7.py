2ebfe669a0d2fab6e8343f06babffac6103d6f88
mdeyanov83 <mdeyanov@gmail.com>
2025-12-13 17:38:16 +0000

    Sat, Dec 13, 2025, 5:38 PM +00:00

--- python_deepdive1/11-extras/008 - argparse/test/example7.py
+++ python_deepdive1/11-extras/008 - argparse/test/example7.py
@@ -3,10 +3,9 @@ import datetime
 
 parser = argparse.ArgumentParser(description='Returns a string containing the name and age of the person')
 
-parser.add_argument('-f', '--first', help='first name', type=str, requried=False)
+parser.add_argument('-f', '--first', help='first name', type=str, required=False)
 parser.add_argument('-l', '--last', help='last name', type=str, required=True)
-parser.add_argument('--yob', help='year of birt', type=int, requried=False)
-
+parser.add_argument('--yob', help='year of birt', type=int, required=False)
 args = parser.parse_args()
 
 print(args)
