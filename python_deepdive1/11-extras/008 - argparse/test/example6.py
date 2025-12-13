e9b27823581180b1627c9645e43f3ee8638dafca
mdeyanov83 <mdeyanov@gmail.com>
2025-12-13 17:29:48 +0000

    Sat, Dec 13, 2025, 5:29 PM +00:00

---
+++ python_deepdive1/11-extras/008 - argparse/test/example6.py
@@ -0,0 +1,13 @@
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Calculates the div a/b and mod a%b of two integers')
+
+parser.add_argument('a', help='first integer', type=int)
+parser.add_argument('b', help='second integer', type=int)
+
+args = parser.parse_args(sys.argv[1:])
+
+
+print(args.a)
+print(args.b)
