5c122f42e162eee584e927c86794219d9f69d842
mdeyanov83 <mdeyanov@gmail.com>
2025-12-13 18:01:06 +0000

    Sat, Dec 13, 2025, 6:01 PM +00:00

--- python_deepdive1/11-extras/008 - argparse/test/example9.py
+++ python_deepdive1/11-extras/008 - argparse/test/example9.py
@@ -2,10 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description='testing defaults and flags')
 
-parser.add_argument('--monty', action='store_const', const='Python')
-parser.add_argument('-n', '--name', default='John')
-
-
+# parser.add_argument('--monty', action='store_const', const='Python')
+# parser.add_argument('-n', '--name', default='John')
+parser.add_argument('-v', '--verbose', action='store_const', const=True, default=False)
+parser.add_argument('-v2', action='store_const', const=True)
 args = parser.parse_args()
 
 print(args)
