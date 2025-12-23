ff6cc1a5245ca24ef82b63f4d3f868e3582923c4
mdeyanov83 <mdeyanov@gmail.com>
2025-12-23 12:04:59 +0000

    Tue, Dec 23, 2025, 12:04 PM +00:00

--- python_deepdive_2/05_project_2/Project 2 Solution.py
+++ python_deepdive_2/05_project_2/Project 2 Solution.py
@@ -160,16 +160,12 @@ class Polygons:
             raise ValueError('m must be greater than 3')
         self._m = m
         self._R = R
-        self._polygons = [Polygon(i, R) for i in range(3, m+1)]
 
-    # def __len__(self):
-    #     return self._m - 2
+    def __len__(self):
+        return self._m - 2
 
     def __repr__(self):
         return f'Polygons(m={self._m}, R={self._R})'
-
-    # def __getitem__(self, s):
-    #     return self._polygons[s]
 
     def __iter__(self):
         return self.PolygonIterator(self)
@@ -183,8 +179,8 @@ class Polygons:
             return self
 
         def __next__(self):
+            ...
 
-            
 
     @property
     def max_efficiency_polygon(self):
