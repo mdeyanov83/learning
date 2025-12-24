f661b3d20d3917c7efacc616caf0b6600ebcb375
mdeyanov83 <mdeyanov@gmail.com>
2025-12-24 12:24:40 +0000

    Wed, Dec 24, 2025, 12:24 PM +00:00

--- python_deepdive_2/07_project_3/Project 3 My Solution.py
+++ python_deepdive_2/07_project_3/Project 3 My Solution.py
@@ -3,9 +3,10 @@ FILE_NAME = 'nyc_parking_tickets_extract.csv'
 
 def parse_data():
     with open(FILE_NAME) as f:
-        print(next(f))
-        print(next(f))
-        print(next(f))
+        print(next(f).strip('\n'))
+        print(next(f).strip('\n'))
+        print(next(f).strip('\n'))
+
 
 
 
