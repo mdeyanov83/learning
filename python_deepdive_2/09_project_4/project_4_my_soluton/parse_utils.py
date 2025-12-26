0de8a71d66802d18031c7ffcf9481fc3be27fb24
mdeyanov83 <mdeyanov@gmail.com>
2025-12-26 14:55:02 +0000

    Fri, Dec 26, 2025, 2:55 PM +00:00

--- python_deepdive_2/09_project_4/project_4_my_soluton/parse_utils.py
+++ python_deepdive_2/09_project_4/project_4_my_soluton/parse_utils.py
@@ -2,3 +2,26 @@ import csv
 from datetime import datetime
 from collections import namedtuple
 
+fname_personal = 'data/personal_info.csv'
+
+
+
+
+def read_data(file):
+    with open(file) as f:
+        reader = csv.reader(f, delimiter=',', quotechar='"')
+
+
+        yield from reader
+
+
+
+def main():
+    data = read_data(fname_personal)
+
+    for _ in range(10):
+        print(next(data))
+
+
+if __name__ == "__main__":
+    main()
