8332e575ea0d5e6e26454298404e03c8c315cd84
mdeyanov83 <mdeyanov@gmail.com>
2025-12-26 15:34:00 +0000

    Fri, Dec 26, 2025, 3:34 PM +00:00

--- python_deepdive_2/09_project_4/project_4_test_all_goals/constants.py
+++ python_deepdive_2/09_project_4/project_4_test_all_goals/constants.py
@@ -12,10 +12,18 @@ fnames = (fname_personal, fname_employment, fname_vehicles, fname_update_status)
 # Parsers
 personal_parser = (str, str, str, str, str)
 employment_parser = (str, str, str, str)
-vehicles_parser = (str, str, str, int)
+vehicle_parser = (str, str, str, int)
 update_status_parser = (str, parse_date, parse_date)
 
-parsers = (personal_parser, employment_parser, vehicles_parser, update_status_parser)
+parsers = (personal_parser, employment_parser, vehicle_parser, update_status_parser)
 
 
 # Named Tuple Names
+personal_class_name = 'Personal'
+employment_class_name = 'Employment'
+vehicle_class_name = 'Vehicle'
+update_status_class_name = 'UpdateStatus'
+
+class_names = (personal_class_name, employment_class_name, vehicle_class_name, update_status_class_name)
+
+
--- python_deepdive_2/09_project_4/project_4_test_all_goals/parse_utils.py
+++ python_deepdive_2/09_project_4/project_4_test_all_goals/parse_utils.py
@@ -10,3 +10,8 @@ def csv_parser(fname, *, delimiter=',', quotechar='"', include_header=False):
 
 def parse_date(value, *, fmt='%Y-%m-%dT%H:%M:%SZ'):
     return datetime.strptime(value, fmt)
+
+
+def extract_field_names(fname):
+    reader = csv_parser(fname, include_header=True)
+    return next(reader)
