aaaf1005c9fa6ce0aeb5e1fc57b141c7a28840b0
mdeyanov83 <mdeyanov@gmail.com>
2025-12-26 19:38:42 +0000

    Fri, Dec 26, 2025, 7:38 PM +00:00

--- python_deepdive_2/09_project_4/project_4_goal_4_test/playground.py
+++ python_deepdive_2/09_project_4/project_4_goal_4_test/playground.py
@@ -50,10 +50,8 @@ from datetime import datetime
 
 cutoff_date = datetime(2017, 3, 11)
 
-
 def group_key(item):
     return item.gender, item.vehicle_make
-
 
 data = parse_utils.filtered_iter_combined(constants.fnames,
                                           constants.class_names,
@@ -63,15 +61,15 @@ data = parse_utils.filtered_iter_combined(constants.fnames,
 
 sorted_data = sorted(data, key=group_key)
 groups = itertools.groupby(sorted_data, key=group_key)
-
 group_1, group_2 = itertools.tee(groups, 2)
 
 group_f = (item for item in group_1 if item[0][0] == 'Female')
+data_f = ((item[0][1], len(list(item[1]))) for item in group_f)
 print('Group F')
-for row in group_f:
+for row in data_f:
     print(row)
 
 group_m = (item for item in group_2 if item[0][0] == 'Male')
-print('Group M')
-for row in group_m:
-    print(row)
+# print('Group M')
+# for row in group_m:
+#     print(row)
