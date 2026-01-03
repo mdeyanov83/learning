03c7df2625b407c028344107ed16c2733c9f7869
mdeyanov83 <mdeyanov@gmail.com>
2026-01-03 19:42:00 +0000

    Sat, Jan 3, 2026, 7:42 PM +00:00

---
+++ JavaScript/various_js/barista.js
@@ -0,0 +1,19 @@
+// If you can read this code, you could
+// probably use a free cup of coffee
+
+var your_drink;
+
+var reverse = function (s) {
+  return s.split("").reverse().join("");
+};
+
+var barista = {
+  str1: "ion",
+  str2: reverse("rcne"),
+  str3: "ypt",
+  request: function (preference) {
+    return preference + "Secret word:" + this.str2 + this.str3 + this.str1;
+  },
+};
+
+console.log(barista.request(your_drink));
