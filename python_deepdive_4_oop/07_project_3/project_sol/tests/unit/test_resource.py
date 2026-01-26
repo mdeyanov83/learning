95985ff2f3f2778c462637b89b7fdb5549a191f7
mdeyanov83 <mdeyanov@gmail.com>
2026-01-26 20:38:09 +0000

    Mon, Jan 26, 2026, 8:38 PM +00:00

--- python_deepdive_4_oop/07_project_3/project_sol/tests/unit/test_resource.py
+++ python_deepdive_4_oop/07_project_3/project_sol/tests/unit/test_resource.py
@@ -8,6 +8,15 @@ import pytest
 
 from app.models import inventory
 
+@pytest.fixture
+def resource_values():
+    return {
+        'name': 'Parrot',
+        'manufacturer': 'Pirates A-Hoy',
+        'total': 100,
+        'allocated': 50
+    }
+
 def test_create_resource():
     resource = inventory.Resource('Parrot', 'Pirates A-Hoy', 100, 50)
     assert resource.name == 'Parrot'
