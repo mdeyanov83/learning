96ba87024d20be44cf0e8d3b8c4b875ffdc6cb98
mdeyanov83 <mdeyanov@gmail.com>
2026-01-26 15:49:04 +0000

    Mon, Jan 26, 2026, 3:49 PM +00:00

---
+++ python_deepdive_4_oop/07_project_3/project_sol/app/utils/validators.py
@@ -0,0 +1,16 @@
+"""Various validators"""
+
+def validate_integer(
+    arg_name, arg_value, min_value=None, max_value=None,
+    custom_min_message=None, custom_max_message=None
+):
+    """
+    Docstring for validate_integer
+
+    :param arg_name: Description
+    :param arg_value: Description
+    :param min_value: Description
+    :param max_value: Description
+    :param custom_min_message: Description
+    :param custom_max_message: Description
+    """
