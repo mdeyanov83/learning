846b8741f0ddee8c0f8bdce6a155bab843a0b65e
mdeyanov83 <mdeyanov@gmail.com>
2026-01-26 20:25:17 +0000

    Mon, Jan 26, 2026, 8:25 PM +00:00

---
+++ python_deepdive_4_oop/07_project_3/project_sol/tests/unit/test_resource.py
@@ -0,0 +1,16 @@
+"""
+Tests for Resource class
+Command line: python -m pytest tests/unit/test_resource.py
+"""
+
+
+import pytest
+
+from app.models import inventory
+
+def test_create_resource():
+    resource = inventory.Resource('Parrot', 'Pirates A-Hoy', 100, 50)
+    assert resource.name == 'Parrot'
+    assert resource.manufacturer == 'Pirates A-Hoy'
+    assert resource.total == 100
+    assert resource.
