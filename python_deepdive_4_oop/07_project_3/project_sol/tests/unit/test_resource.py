93377fcf88a00917e376d9a830da48fcf2b1afd1
mdeyanov83 <mdeyanov@gmail.com>
2026-01-26 20:36:21 +0000

    Mon, Jan 26, 2026, 8:36 PM +00:00

--- python_deepdive_4_oop/07_project_3/project_sol/tests/unit/test_resource.py
+++ python_deepdive_4_oop/07_project_3/project_sol/tests/unit/test_resource.py
@@ -14,3 +14,8 @@ def test_create_resource():
     assert resource.manufacturer == 'Pirates A-Hoy'
     assert resource.total == 100
     assert resource.allocated == 50
+
+
+def test_create_invalid_total_type():
+    with pytest.raises(TypeError):
+        inventory.Resource('Parrot', 'Pirates A-Hoy', 10.5, 5)
