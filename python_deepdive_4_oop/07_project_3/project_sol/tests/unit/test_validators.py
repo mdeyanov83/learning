6a2e0faa5cd7b8f0a395dae61b40b2b1a5d726ed
mdeyanov83 <mdeyanov@gmail.com>
2026-01-26 16:27:53 +0000

    Mon, Jan 26, 2026, 4:27 PM +00:00

---
+++ python_deepdive_4_oop/07_project_3/project_sol/tests/unit/test_validators.py
@@ -0,0 +1,12 @@
+"""
+Tests the validaor functions
+Command line: python -m pytest tests/test_validators.py
+"""
+
+import pytest
+
+from app.utils.validators import validate_integer
+
+class TestIntegerValidator:
+    def test_valid(self):
+        validate_integer('arg', 10, 0, 20, 'custom min msg', 'custom max msg')
