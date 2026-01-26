947301db68c5cbbcf72e41db9fd96251d7d22132
mdeyanov83 <mdeyanov@gmail.com>
2026-01-26 16:09:21 +0000

    Mon, Jan 26, 2026, 4:09 PM +00:00

--- python_deepdive_4_oop/07_project_3/project_sol/app/utils/validators.py
+++ python_deepdive_4_oop/07_project_3/project_sol/app/utils/validators.py
@@ -4,13 +4,36 @@ def validate_integer(
     arg_name, arg_value, min_value=None, max_value=None,
     custom_min_message=None, custom_max_message=None
 ):
-    """
-    Docstring for validate_integer
+    """Validate taht 'arg_value' is an integer, and optionally falls within specific bounds.
+    A custom override error message can be provided when min/max bounds are exceeded.
 
-    :param arg_name: Description
-    :param arg_value: Description
-    :param min_value: Description
-    :param max_value: Description
-    :param custom_min_message: Description
-    :param custom_max_message: Description
+    Args:
+        arg_name (str): the name of the argument (used in default error messages)
+        arg_value (obj): the value being validated
+        min_value (int, optional): specifies minimum value (inclusive). Defaults to None.
+        max_value (int, optional): specifies maximum value (inclusive). Defaults to None.
+        custom_min_message (str, optional): custom message when value is less than minimum. Defaults to None.
+        custom_max_message (str, optional): custom message when value is greater than maximum. Defaults to None.
+
+    Returns:
+        None: no exceptions raised if validation passes
+
+    Raises:
+        TypeError: if `arg_value` is not an integer
+        ValueError: if `arg_value` does not satisfy the bounds
     """
+
+    if not isinstance(arg_value, int):
+        raise TypeError(f'{arg_name} must be an integer.')
+
+    if min_value is not None and arg_value < min_value:
+        if custom_min_message is not None:
+            raise ValueError(custom_min_message)
+        else:
+            raise ValueError(f'{arg_name} cannot be less than {min_value}')
+
+    if max_value is not None and arg_value > max_value:
+        if custom_max_message is not None:
+            raise ValueError(custom_max_message)
+        else:
+            raise ValueError(f'{arg_name} cannot be greater than {max_value}')
