03ad4908515550f94fe3dda515e42e7bd09246c9
mdeyanov83 <mdeyanov@gmail.com>
2026-01-26 19:16:43 +0000

    Mon, Jan 26, 2026, 7:16 PM +00:00

--- python_deepdive_4_oop/07_project_3/project_sol/app/models/inventory.py
+++ python_deepdive_4_oop/07_project_3/project_sol/app/models/inventory.py
@@ -3,15 +3,28 @@
 
 from app.utils.validators import validate_integer
 
+
 class Resource:
     """Base class for resources"""
 
     def __init__(self, name, manufacturer, total, allocated):
-        """_summary_
+        """
 
         Args:
             name (str): display name of recources
-            manufacturer (str): 
-            total (int): _description_
-            allocated (int): _description_
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+
+        Note:
+            `allocated` cannot exceed `total`
         """
+
+        self._name = name
+        self._manufacturer = manufacturer
+
+        validate_integer('total', total, min_value=0)
+        self._total = total
+
+        validate_integer('allocated', allocated, 0, total,
+                         custom_max_message='Allocated inventory cannot exceed total inventory')
