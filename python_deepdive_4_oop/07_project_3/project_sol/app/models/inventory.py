d95916ab615dc52f37d5e8ce67cafc1ac562a48d
mdeyanov83 <mdeyanov@gmail.com>
2026-01-26 20:33:01 +0000

    Mon, Jan 26, 2026, 8:33 PM +00:00

--- python_deepdive_4_oop/07_project_3/project_sol/app/models/inventory.py
+++ python_deepdive_4_oop/07_project_3/project_sol/app/models/inventory.py
@@ -32,118 +32,118 @@ class Resource:
         )
         self._allocated = allocated
 
-        @property
-        def name(self):
-            """
-            Retruns:
-                str: the resources name
-            """
-            return self._name
+    @property
+    def name(self):
+        """
+        Retruns:
+            str: the resources name
+        """
+        return self._name
 
-        @property
-        def manufacturer(self):
-            """
-            Returns:
-                str: the resource manufacturer
-            """
-            return self._manufacturer
+    @property
+    def manufacturer(self):
+        """
+        Returns:
+            str: the resource manufacturer
+        """
+        return self._manufacturer
 
-        @property
-        def total(self):
-            """
-            Returns:
-                int: the total inventory count
-            """
-            return self._total
+    @property
+    def total(self):
+        """
+        Returns:
+            int: the total inventory count
+        """
+        return self._total
 
-        @property
-        def allocated(self):
-            """
-            Returns:
-                int: number of resources in use
-            """
-            return self._allocated
+    @property
+    def allocated(self):
+        """
+        Returns:
+            int: number of resources in use
+        """
+        return self._allocated
 
-        @property
-        def category(self):
-            """
-            Returns:
-                str: the resource category
-            """
-            return type(self).__name__.lower()
+    @property
+    def category(self):
+        """
+        Returns:
+            str: the resource category
+        """
+        return type(self).__name__.lower()
 
-        @property
-        def available(self):
-            """
-            Returns:
-                int: number of resources available for use
-            """
-            return self.total - self.allocated
+    @property
+    def available(self):
+        """
+        Returns:
+            int: number of resources available for use
+        """
+        return self.total - self.allocated
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
 
-        def __repr__(self):
-            return (f'{self.name} ({self.category} - {self.manufacturer}) : '
-                    f'total={self.total}, allocated={self.allocated}')
+    def __repr__(self):
+        return (f'{self.name} ({self.category} - {self.manufacturer}) : '
+                f'total={self.total}, allocated={self.allocated}')
 
-        def claim(self, num):
-            """
-            Claim num inventory items (if available)
+    def claim(self, num):
+        """
+        Claim num inventory items (if available)
 
-            Args:
-                num (int): Number of inventory items to claim
+        Args:
+            num (int): Number of inventory items to claim
 
-            Returns:
-            """
-            validate_integer(
-                'num', num, 1, self.available,
-                custom_max_message='Cannot claim more than available'
-            )
-            self._allocated += num
+        Returns:
+        """
+        validate_integer(
+            'num', num, 1, self.available,
+            custom_max_message='Cannot claim more than available'
+        )
+        self._allocated += num
 
-        def freeup(self, num):
-            """
-            Return an inventory item to the available pool
+    def freeup(self, num):
+        """
+        Return an inventory item to the available pool
 
-            Args:
-                num (int): Number of items to return (cannot exceed number in use)
+        Args:
+            num (int): Number of items to return (cannot exceed number in use)
 
-            Returns:
+        Returns:
 
-            """
-            validate_integer(
-                'num', num, 1, self.allocated,
-                custom_max_message='Cannot return more than allocated'
-            )
-            self._allocated -= num
+        """
+        validate_integer(
+            'num', num, 1, self.allocated,
+            custom_max_message='Cannot return more than allocated'
+        )
+        self._allocated -= num
 
-        def died(self, num):
-            """
-            Number of items to deallocate and remove from the inventory pool altogether
+    def died(self, num):
+        """
+        Number of items to deallocate and remove from the inventory pool altogether
 
-            Args:
-                num (nit): Number of items that have died
+        Args:
+            num (nit): Number of items that have died
 
-            Returns:
+        Returns:
 
-            """
-            validate_integer(
-                'num', num, 1, self.allocated,
-                custom_max_message='Cannot retire more than allocated'
-            )
-            self._total -= num
-            self._allocated -= num
+        """
+        validate_integer(
+            'num', num, 1, self.allocated,
+            custom_max_message='Cannot retire more than allocated'
+        )
+        self._total -= num
+        self._allocated -= num
 
-        def purchased(self, num):
-            """
-            Add new inventory to the pool.
+    def purchased(self, num):
+        """
+        Add new inventory to the pool.
 
-            Args:
-                num (int): Number of itesm to add to the pool
+        Args:
+            num (int): Number of itesm to add to the pool
 
-            Returns:
+        Returns:
 
-            """
-            validate_integer('num', num, 1)
-            self._total += num
+        """
+        validate_integer('num', num, 1)
+        self._total += num
