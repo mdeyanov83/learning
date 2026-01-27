a3b35602d81f5e2229876626f23f4e06c2907911
mdeyanov83 <mdeyanov@gmail.com>
2026-01-27 00:50:33 +0000

    Tue, Jan 27, 2026, 12:50 AM +00:00

--- python_deepdive_4_oop/07_project_3/project_sol/app/models/inventory.py
+++ python_deepdive_4_oop/07_project_3/project_sol/app/models/inventory.py
@@ -247,3 +247,96 @@ class Storage(Resource):
 
     def __repr__(self):
         return f'{self.category}: {self.capacity_gb} GB'
+
+
+
+class HDD(Storage):
+    """
+    Class used for HDD type resources
+    """
+    def __init__(
+            self, name, manufacturer, total, allocated, capacity_gb,
+            size, rpm
+    ):
+        """
+
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            capacity_gb (int): storage capacity (in GB)
+            size (str): indicates the device size (must be either 2.5" or 3.5")
+            rpm (int): disk rotation speed (in rpm)
+        """
+        super().__init__(name, manufacturer, total, allocated, capacity_gb)
+
+        allowed_sizes = ['2.5"', '3.5"']
+        if size not in allowed_sizes:
+            raise ValueError(f'Invalid HDD size. '
+                             f'Must be one of {", ".join(allowed_sizes)}')
+        validate_integer('rpm', rpm, min_value=1_000, max_value=50_000)
+
+        self._size = size
+        self._rpm = rpm
+
+    @property
+    def size(self):
+        """
+        The HDD size (2.5" / 3.5")
+
+        Returns:
+            str
+        """
+        return self._size
+
+    @property
+    def rpm(self):
+        """
+        The HDD spin speed (rpm)
+
+        Returns:
+            int
+        """
+        return self._rpm
+
+    def __repr__(self):
+        s = super().__repr__()
+        return f'{s} ({self.size}, {self.rpm} rpm)'
+
+
+class SSD(Storage):
+    """
+    Class used for SSD type resources
+    """
+    def __init__(
+            self, name, manufacturer, total, allocated, capacity_gb,
+            interface
+    ):
+        """
+
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            capacity_gb (int): storage capacity (in GB)
+            interface (str): indicates the device interface (e.g. PCIe NVMe 3.0 x4)
+        """
+        super().__init__(name, manufacturer, total, allocated, capacity_gb)
+
+        self._interface = interface
+
+    @property
+    def interface(self):
+        """
+        Interface used by SSD (e.g. PCIe NVMe 3.0 x4)
+
+        Returns:
+            str
+        """
+        return self._interface
+
+    def __repr__(self):
+        s = super().__repr__()
+        return f'{s} ({self.interface})'
