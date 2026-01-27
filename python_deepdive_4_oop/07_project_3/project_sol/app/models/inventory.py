fc7a39c19111c60019c1c94279622304ce7dcff2
mdeyanov83 <mdeyanov@gmail.com>
2026-01-27 00:46:50 +0000

    Tue, Jan 27, 2026, 12:46 AM +00:00

--- python_deepdive_4_oop/07_project_3/project_sol/app/models/inventory.py
+++ python_deepdive_4_oop/07_project_3/project_sol/app/models/inventory.py
@@ -147,3 +147,103 @@ class Resource:
         """
         validate_integer('num', num, 1)
         self._total += num
+
+
+
+class CPU(Resource):
+    """Resource subclass used to track specific CPU inventory pools"""
+
+    def __init__(
+            self, name, manufacturer, total, allocated,
+            cores, socket, power_watts
+    ):
+        """
+
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            cores (int): number of cores
+            socket (str): CPU socket type
+            power_watts (int): CPU rated wattage
+        """
+        super().__init__(name, manufacturer, total, allocated)
+
+        validate_integer('cores', cores, 1)
+        validate_integer('power_watts', power_watts, 1)
+
+        self._cores = cores
+        self._socket = socket
+        self._power_watts = power_watts
+
+    @property
+    def cores(self):
+        """
+        Number of cores.
+
+        Returns:
+            int
+        """
+        return self._cores
+
+    @property
+    def socket(self):
+        """
+        The socket type for this CPU
+
+        Returns:
+            str
+        """
+        return self._socket
+
+
+    @property
+    def power_watts(self):
+        """
+        The rated wattage of this CPU
+
+        Returns:
+            int
+        """
+        return self._power_watts
+
+
+    def __repr__(self):
+        return f'{self.category}: {self.name} {self.socket} - x{self.cores})'
+
+
+
+class Storage(Resource):
+    """
+    A base class for storage devices - probably not used directly
+    """
+
+    def __init__(self, name, manufacturer, total, allocated, capacity_gb):
+        """
+
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            capacity_gb (int): storage capacity (in GB)
+        """
+        super().__init__(name, manufacturer, total, allocated)
+        validate_integer('capacity_gb', capacity_gb, 1)
+        self._capacity_gb = capacity_gb
+
+
+    @property
+    def capacity_gb(self):
+        """
+        Indicates the capacity (in GB) of the storage device
+
+        Returns:
+            int
+        """
+        return self._capacity_gb
+
+
+    def __repr__(self):
+        return f'{self.category}: {self.capacity_gb} GB'
